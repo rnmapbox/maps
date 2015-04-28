0aeee37899ca895fc2d83670db48ff78cdb82303
rsudekum <bobby@mapbox.com>
2015-04-28 10:36:25 -0700

    add download preinstall step

---
+++ scripts/download-mapbox-gl-native-ios.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+VERSION=$1
+
+if ! which curl > /dev/null; then echo "curl command not found. Please install curl"; exit 1; fi;
+if ! which unzip > /dev/null; then echo "unzip command not found. Please install unzip"; exit 1; fi;
+
+mkdir temp
+
+curl -sS http://mapbox.s3.amazonaws.com/mapbox-gl-native/ios/builds/mapbox-gl-ios-$VERSION.zip > temp.zip
+unzip -o temp.zip -d temp
+mv libMapboxGL.a .
+rm -r temp
+rm temp.zip
