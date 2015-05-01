7534672b3c07d99ab9db0c195974ec37d5b24b68
rsudekum <bobby@mapbox.com>
2015-04-30 22:11:29 -0700

    Make project into requirable module

---
+++ RCTMapboxGL/RCTMapboxGL.m
@@ -0,0 +1,13 @@
+//
+//  RCTMapboxGL.m
+//  RCTMapboxGL
+//
+//  Created by Bobby Sudekum on 4/30/15.
+//  Copyright (c) 2015 Mapbox. All rights reserved.
+//
+
+#import "RCTMapboxGL.h"
+
+@implementation RCTMapboxGL
+
+@end
