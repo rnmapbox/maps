55874e28f2e97e1b37a251eb4c029fde68e34d1b
Brent Vatne <brent.vatne@madriska.com>
2015-05-10 12:56:16 -0700

    Various bugfixes as discussed in IRC

--- RCTMapboxGL/RCTMapboxGL.h
+++ RCTMapboxGL/RCTMapboxGL.h
@@ -7,15 +7,32 @@
 //
 
 #import "MapboxGL.h"
+#import "MGLMapView.h"
+#import "RCTView.h"
+#import "RCTEventDispatcher.h"
 
-@interface RCTMapboxGL : MGLMapView
+@interface RCTMapboxGL : RCTView <MGLMapViewDelegate>
+
+- (instancetype)initWithEventDispatcher:(RCTEventDispatcher *)eventDispatcher;
+
+- (void)setAccessToken:(NSString *)accessToken;
+- (void)setAnnotations:(NSMutableDictionary *)annotations;
+- (void)setCenterCoordinate:(CLLocationCoordinate2D)centerCoordinate;
+- (void)setClipsToBounds:(BOOL)clipsToBounds;
+- (void)setDebugActive:(BOOL)debugActive;
+- (void)setDirection:(double)direction;
+- (void)setRotateEnabled:(BOOL)rotateEnabled;
+- (void)setShowsUserLocation:(BOOL)showsUserLocation;
+- (void)setStyleURL:(NSURL *)styleURL;
+- (void)setZoomLevel:(double)zoomLevel;
 
 @end
 
-@interface addGLAnnotation : NSObject <MGLAnnotation>
+@interface MGLAnnotation : NSObject <MGLAnnotation>
 
 + (instancetype)annotationWithLocation:(CLLocationCoordinate2D)coordinate title:(NSString *)title subtitle:(NSString *)subtitle;
 
 - (instancetype)initWithLocation:(CLLocationCoordinate2D)coordinate title:(NSString *)title subtitle:(NSString *)subtitle;
+
 
 @end
