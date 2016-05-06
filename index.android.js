4128db9b2938213c01c46d4da5953c970ec3a5e3
Bobby Sudekum <bobby@mapbox.com>
2016-05-06 14:10:16 -0700

    Cleanup android (#347)

--- index.android.js
+++ index.android.js
@@ -2,13 +2,9 @@
 
 var React = require('react');
 var { PropTypes } = React;
-
 var ReactNative = require('react-native');
-var { NativeModules, requireNativeComponent, findNodeHandle, DeviceEventEmitter } = ReactNative;
-
+var { NativeModules, requireNativeComponent, findNodeHandle } = ReactNative;
 var { MapboxGLManager } = NativeModules;
-
-var Subscribable = require('Subscribable');
 
 var MapMixins = {
   setDirectionAnimated(mapRef, heading) {
@@ -57,93 +53,62 @@ var MapMixins = {
   userTrackingMode: MapboxGLManager.userTrackingMode
 };
 
-var ReactMapView = requireNativeComponent('RCTMapbox', {
-    name: 'RCTMapbox',
-    propTypes: {
-      accessToken: PropTypes.string.isRequired,
-      attributionButtonIsHidden: PropTypes.bool,
-      logoIsHidden: PropTypes.bool,
-      annotations: PropTypes.arrayOf(PropTypes.shape({
-        title: PropTypes.string,
-        subtitle: PropTypes.string,
-        coordinates: PropTypes.array.isRequired,
-        alpha: PropTypes.number,
-        fillColor: PropTypes.string,
-        strokeColor: PropTypes.string,
-        strokeWidth: PropTypes.number
-      })),
-      centerCoordinate: PropTypes.shape({
-        latitude: PropTypes.number.isRequired,
-        longitude: PropTypes.number.isRequired
-      }),
-      centerCoordinateZoom: PropTypes.shape(),
-      debugActive: PropTypes.bool,
-      direction: PropTypes.number,
-      rotateEnabled: PropTypes.bool,
-      scrollEnabled: PropTypes.bool,
-      showsUserLocation: PropTypes.bool,
-      styleURL: PropTypes.string,
-      userTrackingMode: PropTypes.number,
-      zoomEnabled: PropTypes.bool,
-      zoomLevel: PropTypes.number,
-      tilt: PropTypes.number,
-      compassIsHidden: PropTypes.bool,
-      onRegionChange: PropTypes.func,
-      onOpenAnnotation: PropTypes.func,
-      onLongPress: PropTypes.func,
-      onUserLocationChange: PropTypes.func,
-      // Fix for https://github.com/mapbox/react-native-mapbox-gl/issues/118
-      scaleY: PropTypes.number,
-      scaleX: PropTypes.number,
-      translateY: PropTypes.number,
-      translateX: PropTypes.number,
-      rotation: PropTypes.number,
-      // Fix for https://github.com/mapbox/react-native-mapbox-gl/issues/175
-      renderToHardwareTextureAndroid: PropTypes.bool,
-      onLayout: PropTypes.bool,
-      accessibilityLiveRegion: PropTypes.string,
-      accessibilityComponentType: PropTypes.string,
-      accessibilityLabel: PropTypes.string,
-      testID: PropTypes.string,
-      importantForAccessibility: PropTypes.string
-    },
-    defaultProps() {
-      return {
-        centerCoordinate: {
-          latitude: 0,
-          longitude: 0
-        },
-        debugActive: false,
-        direction: 0,
-        rotateEnabled: true,
-        scrollEnabled: true,
-        showsUserLocation: false,
-        styleURL: MapboxGLManager.mapStyles.streets,
-        userTrackingMode: MapboxGLManager.userTrackingMode.none,
-        zoomEnabled: true,
-        zoomLevel: 0,
-        tilt: 0,
-        compassIsHidden: false
-      };
-    }
-});
-
 var ReactMapViewWrapper = React.createClass({
-  mixins: [Subscribable.Mixin],
   statics: {
     Mixin: MapMixins
   },
-  propTypes: {
-    onRegionChange: PropTypes.func,
-    onUserLocationChange: PropTypes.func,
-    onOpenAnnotation: PropTypes.func,
-    onLongPress: PropTypes.func
+  defaultProps() {
+    return {
+      centerCoordinate: {
+        latitude: 0,
+        longitude: 0
+      },
+      debugActive: false,
+      direction: 0,
+      rotateEnabled: true,
+      scrollEnabled: true,
+      showsUserLocation: false,
+      styleURL: MapboxGLManager.mapStyles.streets,
+      userTrackingMode: MapboxGLManager.userTrackingMode.none,
+      zoomEnabled: true,
+      zoomLevel: 0,
+      tilt: 0,
+      compassIsHidden: false
+    };
   },
-  componentWillMount: function() {
-    this.addListenerOn(DeviceEventEmitter,'onRegionChange', this.handleOnChange);
-    this.addListenerOn(DeviceEventEmitter,'onUserLocationChange', this.handleUserLocation);
-    this.addListenerOn(DeviceEventEmitter,'onOpenAnnotation', this.handleOnOpenAnnotation);
-    this.addListenerOn(DeviceEventEmitter,'onLongPress', this.handleOnLongPress);
+  propTypes: {
+    accessToken: PropTypes.string.isRequired,
+    attributionButtonIsHidden: PropTypes.bool,
+    logoIsHidden: PropTypes.bool,
+    annotations: PropTypes.arrayOf(PropTypes.shape({
+      title: PropTypes.string,
+      subtitle: PropTypes.string,
+      coordinates: PropTypes.array.isRequired,
+      alpha: PropTypes.number,
+      fillColor: PropTypes.string,
+      strokeColor: PropTypes.string,
+      strokeWidth: PropTypes.number
+    })),
+    centerCoordinate: PropTypes.shape({
+      latitude: PropTypes.number.isRequired,
+      longitude: PropTypes.number.isRequired
+    }),
+    centerCoordinateZoom: PropTypes.shape(),
+    debugActive: PropTypes.bool,
+    direction: PropTypes.number,
+    rotateEnabled: PropTypes.bool,
+    scrollEnabled: PropTypes.bool,
+    showsUserLocation: PropTypes.bool,
+    styleURL: PropTypes.string,
+    userTrackingMode: PropTypes.number,
+    zoomEnabled: PropTypes.bool,
+    zoomLevel: PropTypes.number,
+    tilt: PropTypes.number,
+    compassIsHidden: PropTypes.bool,
+    onRegionChange: PropTypes.func,
+    onOpenAnnotation: PropTypes.func,
+    onLongPress: PropTypes.func,
+    onUserLocationChange: PropTypes.func
   },
   handleOnChange(event) {
     if (this.props.onRegionChange) this.props.onRegionChange(event);
@@ -160,9 +125,15 @@ var ReactMapViewWrapper = React.createClass({
   render() {
     return (
       <ReactMapView
+        onRegionChange={this.handleOnChange}
+        onUserLocationChange={this.handleUserLocation}
+        onOpenAnnotation={this.handleOnOpenAnnotation}
+        onLongPress={this.handleOnLongPress}
         {...this.props} />
     );
   }
 });
+
+var ReactMapView = requireNativeComponent('RCTMapbox');
 
 module.exports = ReactMapViewWrapper;
