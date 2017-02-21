4453983022f1e983579697cd54e5c74bd2115af1
Peter Theill <peter@theill.com>
2017-02-21 21:45:06 +0100

    Add support for paths with spaces (#505)
    If script is being run from a path having spaces it will fail

--- scripts/download-mapbox-gl-native-ios-if-on-mac.js
+++ scripts/download-mapbox-gl-native-ios-if-on-mac.js
@@ -6,7 +6,7 @@ var path = require('path');
 // only download iOS SDK if on Mac OS
 if (process.platform === 'darwin') {
   var exec = require('child_process').exec;
-  var cmd = path.join(__dirname, 'download-mapbox-gl-native-ios.sh') + ' ' + version;
+  var cmd = `"${path.join(__dirname, 'download-mapbox-gl-native-ios.sh')}" ${version}`;
   exec(cmd, function(error, stdout, stderr) {
     if (error) {
       console.error(error);
