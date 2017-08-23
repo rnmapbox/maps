ab8600cede0b7f4283ba489f8730c9b769c431f2
Nick <nick.italiano@mapbox.com>
2017-08-22 17:22:55 -0700

    Added start of android rewrite

---
+++ android/rctmgl/src/main/java/mapbox/rctmgl/RCTMGLPackage.java
@@ -0,0 +1,42 @@
+package mapbox.rctmgl;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by nickitaliano on 8/18/17.
+ */
+
+public class RCTMGLPackage implements ReactPackage {
+
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactApplicationContext) {
+        List<NativeModule> modules = new ArrayList<>();
+
+        modules.add(new RCTMGLModule(reactApplicationContext));
+
+        return modules;
+    }
+
+    @Deprecated
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactApplicationContext) {
+        List<ViewManager> managers = new ArrayList<>();
+
+        managers.add(new RCTMGLMapViewManager(reactApplicationContext));
+
+        return managers;
+    }
+}
