adc0bb01b02a8c73dd709458b920cf8b1f5360d6
Nick <nick.italiano@mapbox.com>
2017-08-24 13:17:22 -0700

    Added eslint

--- javascript/components/MapView.js
+++ javascript/components/MapView.js
@@ -6,26 +6,56 @@ const RCTMGLMapView = requireNativeComponent('RCTMGLMapView', MapView);
 
 const DEFAULT_CENTER_COORDINATE = {
   type: 'Point',
-  coordinates: [-77.036086, 38.910233]
-}
+  coordinates: [-77.036086, 38.910233],
+};
 
+/**
+ * MapView backed by Mapbox Native GL
+ */
 class MapView extends React.Component {
   static StyleURL = {
     Street: 'mapbox-streets',
     Dark: 'mapbox-dark',
     Light: 'mapbox-light',
     Outdoors: 'mapbox-outdoors',
-    Satellite: 'mapbox-satellite'
-  }
+    Satellite: 'mapbox-satellite',
+  };
 
   static propTypes = {
+    /**
+     * Animates changes between pitch and bearing
+     */
     animated: PropTypes.bool,
+
+    /**
+     * Initial center coordinate on map
+     */
     centerCoordinate: PropTypes.object,
+
+    /**
+     * Initial heading on map
+     */
     heading: PropTypes.number,
-    ptich: PropTypes.number,
+
+    /**
+     * Initial pitch on map
+     */
+    pitch: PropTypes.number,
+
+    /**
+     * Style for wrapping React Native View
+     */
     style: PropTypes.any,
+
+    /**
+     * Style URL for map
+     */
     styleURL: PropTypes.string,
-    zoomLevel: PropTypes.number
+
+    /**
+     * Initial zoom level of map
+     */
+    zoomLevel: PropTypes.number,
   };
 
   static defaultProps = {
@@ -34,7 +64,7 @@ class MapView extends React.Component {
     heading: 0,
     pitch: 0,
     zoomLevel: 16,
-    styleURL: MapView.StyleURL.Street
+    styleURL: MapView.StyleURL.Street,
   };
 
   render () {
