fdbebef6b91039a8581634428802e8faed118d82
Nick <nick.italiano@mapbox.com>
2017-09-26 13:18:45 -0700

    Added support for background layer

---
+++ ios/RCTMGL/RCTMGLBackgroundLayer.h
@@ -0,0 +1,14 @@
+//
+//  RCTMGLBackgroundLayer.h
+//  RCTMGL
+//
+//  Created by Nick Italiano on 9/25/17.
+//  Copyright © 2017 Mapbox Inc. All rights reserved.
+//
+
+#import "RCTLayer.h"
+@import Mapbox;
+
+@interface RCTMGLBackgroundLayer : RCTLayer
+
+@end
