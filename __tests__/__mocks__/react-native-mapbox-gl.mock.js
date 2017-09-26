8584acad5f8e382e9c3ba9b088e6002e009e5501
Nick <nick.italiano@mapbox.com>
2017-09-26 15:53:01 -0700

    Added all style enums as constants on module

--- __tests__/__mocks__/react-native-mapbox-gl.mock.js
+++ __tests__/__mocks__/react-native-mapbox-gl.mock.js
@@ -53,6 +53,94 @@ jest.mock('NativeModules', () => {
       'Interval',
       'Identity',
     ]),
+    LineJoin: keyMirror([
+      'Bevel',
+      'Round',
+      'Miter',
+    ]),
+    LineCap: keyMirror([
+      'Butt',
+      'Round',
+      'Square',
+    ]),
+    LineTranslateAnchor: keyMirror([
+      'Map',
+      'Viewport',
+    ]),
+    CirclePitchScale: keyMirror([
+      'Map',
+      'Viewport',
+    ]),
+    CircleTranslateAnchor: keyMirror([
+      'Map',
+      'Viewport',
+    ]),
+    FillExtrusionTranslateAnchor: keyMirror([
+      'Map',
+      'Viewport',
+    ]),
+    FillTranslateAnchor: keyMirror([
+      'Map',
+      'Viewport',
+    ]),
+    IconRotationAlignment: keyMirror([
+      'Auto',
+      'Map',
+      'Viewport',
+    ]),
+    IconTextFit: keyMirror([
+      'None',
+      'Width',
+      'Height',
+      'Both',
+    ]),
+    IconTranslateAnchor: keyMirror([
+      'Map',
+      'Viewport',
+    ]),
+    SymbolPlacement: keyMirror([
+      'Line',
+      'Point',
+    ]),
+    TextAnchor: keyMirror([
+      'Center',
+      'Left',
+      'Right',
+      'Top',
+      'Bottom',
+      'TopLeft',
+      'TopRight',
+      'BottomLeft',
+      'BottomRight',
+    ]),
+    TextJustify: keyMirror([
+      'Center',
+      'Left',
+      'Right',
+    ]),
+    TextPitchAlignment: keyMirror([
+      'Auto',
+      'Map',
+      'Viewport',
+    ]),
+    TextRotationAlignment: keyMirror([
+      'Auto',
+      'Map',
+      'Viewport',
+    ]),
+    TextTransform: keyMirror([
+      'None',
+      'Lowercase',
+      'Uppercase',
+    ]),
+    TextTranslateAnchor: keyMirror([
+      'Map',
+      'Viewport',
+    ]),
+    LightAnchor: keyMirror([
+      'Map',
+      'Viewport',
+    ]),
 
     // methods
     setAccessToken: jest.fn(),
