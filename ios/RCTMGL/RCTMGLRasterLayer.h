009e3c03f8de92576f2ad6a0148c21c0da10a172
Nick <nick.italiano@mapbox.com>
2017-09-26 13:22:19 -0700

    Added support for RasterLayer and RasterSource

---
+++ ios/RCTMGL/RCTMGLRasterLayer.h
@@ -0,0 +1,14 @@
+//
+//  RCTMGLRasterLayer.h
+//  RCTMGL
+//
+//  Created by Nick Italiano on 9/25/17.
+//  Copyright © 2017 Mapbox Inc. All rights reserved.
+//
+
+#import "RCTLayer.h"
+@import Mapbox;
+
+@interface RCTMGLRasterLayer : RCTLayer
+
+@end
