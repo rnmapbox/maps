5bd1b5866ad0d73fed2a27c044024f766ce7364b
Nick <nick.italiano@mapbox.com>
2017-10-19 16:18:40 -0700

    Added example resources

--- example/src/components/ShapeSourceIcon.js
+++ example/src/components/ShapeSourceIcon.js
@@ -43,6 +43,17 @@ const featureCollection = {
         coordinates: [-117.205908, 52.180843],
       },
     },
+    {
+      type: 'Feature',
+      id: '9d10456e-bdda-4aa9-9269-04c1667d4552',
+      properties: {
+        icon: 'pin',
+      },
+      geometry: {
+        type: 'Point',
+        coordinates: [-117.206562, 52.180797],
+      },
+    },
   ],
 };
 
@@ -55,14 +66,14 @@ class ShapeSourceIcon extends React.Component {
     return (
       <Page {...this.props}>
         <MapboxGL.MapView
-            zoomLevel={18}
+            zoomLevel={17}
             centerCoordinate={[-117.20611157485, 52.180961084261]}
             style={sheet.matchParent}>
 
             <MapboxGL.ShapeSource
               id='exampleShapeSource'
               shape={featureCollection}
-              images={{ example: exampleIcon }}>
+              images={{ example: exampleIcon, assets: ['pin'] }}>
               <MapboxGL.SymbolLayer id='exampleIconName' style={styles.icon} />
             </MapboxGL.ShapeSource>
 
