b4ed3f229e8c6e8710ad8ffe44c72daf2e109a96
Nick <nick.italiano@mapbox.com>
2017-11-10 14:32:13 -0800

    Stops adding annotations to map until react frame is set

--- ios/RCTMGL/RCTMGLPointAnnotation.m
+++ ios/RCTMGL/RCTMGLPointAnnotation.m
@@ -15,6 +15,7 @@ const float CENTER_Y_OFFSET_BASE = -0.5f;
 
 @implementation RCTMGLPointAnnotation
 {
+    BOOL isMapSetBeforeFrame;
     UITapGestureRecognizer *customViewTap;
 }
 
@@ -49,6 +50,10 @@ const float CENTER_Y_OFFSET_BASE = -0.5f;
 {
     [self _setCenterOffset:frame];
     [super reactSetFrame:frame];
+
+    if (isMapSetBeforeFrame) {
+        [self _addAnnotation];
+    }
 }
 
 - (void)setAnchor:(NSDictionary<NSString *, NSNumber *> *)anchor
@@ -64,18 +69,22 @@ const float CENTER_Y_OFFSET_BASE = -0.5f;
         _map = nil;
     } else {
         _map = map;
-        [_map addAnnotation:self];
-        
-        if (_reactSelected) {
-            [_map selectAnnotation:self animated:NO];
+
+        // prevents annotations from flying in from the top left corner
+        // if the frame hasn't been set yet
+        if (![self _isFrameSet]) {
+            isMapSetBeforeFrame = YES;
+            return;
         }
+
+        [self _addAnnotation];
     }
 }
 
 - (void)setReactSelected:(BOOL)reactSelected
 {
     _reactSelected = reactSelected;
-    
+
     if (_map != nil) {
         if (_reactSelected) {
             [_map selectAnnotation:self animated:NO];
@@ -141,45 +150,63 @@ const float CENTER_Y_OFFSET_BASE = -0.5f;
     if (frame.size.width == 0 || frame.size.height == 0 || _anchor == nil) {
         return;
     }
-    
+
     float x = [_anchor[@"x"] floatValue];
     float y = [_anchor[@"y"] floatValue];
-    
+
     // (fullWidthOffset - centerWidthOffset) / 2
     float dx = -(x * frame.size.width - (frame.size.width / 2)) / 2;
     float dy = -(y * frame.size.height - (frame.size.height / 2)) / 2;
-    
+
     // special cases 0 and 1
-    
+
     if (x == 0) {
         dx = frame.size.width / 2;
     } else if (x == 1) {
         dy = -frame.size.height / 2;
     }
-    
+
     if (y == 0) {
         dy = frame.size.width / 2;
     } else if (y == 1) {
         dy = -frame.size.height / 2;
     }
-    
+
     self.centerOffset = CGVectorMake(dx, dy);
 }
+
 
 - (void)_updateCoordinate
 {
     if (_reactCoordinate == nil) {
         return;
     }
-    
+
     MGLPointFeature *feature = (MGLPointFeature *)[RCTMGLUtils shapeFromGeoJSON:_reactCoordinate];
     if (feature == nil) {
         return;
     }
-    
+
     dispatch_async(dispatch_get_main_queue(), ^{
        self.coordinate = feature.coordinate;
     });
+}
+
+- (void)_addAnnotation
+{
+    if ([_map.annotations containsObject:self]) {
+        return;
+    }
+
+    [_map addAnnotation:self];
+    if (_reactSelected) {
+        [_map selectAnnotation:self animated:NO];
+    }
+}
+
+- (BOOL)_isFrameSet
+{
+    return self.frame.size.width > 0 && self.frame.size.height > 0;
 }
 
 @end
