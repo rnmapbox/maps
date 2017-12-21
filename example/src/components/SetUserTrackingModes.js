22cdfcd0ad14941cce7567efdbcc89046b1e54b2
Nick <nick.italiano@mapbox.com>
2017-12-20 16:17:54 -0800

    Update example to add userTrackingMode change callback

--- example/src/components/SetUserTrackingModes.js
+++ example/src/components/SetUserTrackingModes.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Text } from 'react-native';
 import MapboxGL from '@mapbox/react-native-mapbox-gl';
 
 import BaseExamplePropTypes from './common/BaseExamplePropTypes';
 import TabBarPage from './common/TabBarPage';
+import Bubble from './common/Bubble';
 
 import sheet from '../styles/sheet';
 import { onSortOptions } from '../utils';
@@ -23,14 +25,37 @@ class SetUserTrackingModes extends React.Component {
     }).sort(onSortOptions);
 
     this.state = {
-      userTrackingMode: this._trackingOptions[0].data,
+      userSelectedUserTrackingMode: this._trackingOptions[0].data,
+      currentTrackingMode: this._trackingOptions[0].data,
     };
 
     this.onTrackingChange = this.onTrackingChange.bind(this);
+    this.onUserTrackingModeChange = this.onUserTrackingModeChange.bind(this);
   }
 
   onTrackingChange (index, userTrackingMode) {
-    this.setState({ userTrackingMode: userTrackingMode });
+    this.setState({
+      userSelectedUserTrackingMode: userTrackingMode,
+      currentTrackingMode: userTrackingMode,
+    });
+  }
+
+  onUserTrackingModeChange (e) {
+    const userTrackingMode = e.nativeEvent.payload.userTrackingMode;
+    this.setState({ currentTrackingMode: userTrackingMode });
+  }
+
+  get userTrackingModeText () {
+    switch (this.state.currentTrackingMode) {
+      case MapboxGL.UserTrackingModes.Follow:
+        return 'Follow';
+      case MapboxGL.UserTrackingModes.FollowWithCourse:
+        return 'FollowWithCourse';
+      case MapboxGL.UserTrackingModes.FollowWithHeading:
+        return 'FolloWithHeading';
+      default:
+        return 'None';
+    }
   }
 
   render () {
@@ -38,8 +63,13 @@ class SetUserTrackingModes extends React.Component {
       <TabBarPage {...this.props} scrollable options={this._trackingOptions} onOptionPress={this.onTrackingChange}>
         <MapboxGL.MapView
             showUserLocation={true}
-            userTrackingMode={this.state.userTrackingMode}
+            userTrackingMode={this.state.userSelectedUserTrackingMode}
+            onUserTrackingModeChange={this.onUserTrackingModeChange}
             style={sheet.matchParent} />
+
+        <Bubble style={{ marginBottom: 100 }}>
+          <Text>User Tracking Mode: {this.userTrackingModeText}</Text>
+        </Bubble>
       </TabBarPage>
     );
   }
