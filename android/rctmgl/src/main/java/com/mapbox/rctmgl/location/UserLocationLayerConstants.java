5e79678db982e09a6345aec5bdc819b7449bd403
Nick <nick.italiano@mapbox.com>
2018-01-08 10:21:41 -0800

    [Android] Insert layers below user location layer
    This adds support for inserting layers below the user location layer, if
    layerIndex, belowLayerID, and addLayerID are not specified from the JS layer

---
+++ android/rctmgl/src/main/java/com/mapbox/rctmgl/location/UserLocationLayerConstants.java
@@ -0,0 +1,13 @@
+package com.mapbox.rctmgl.location;
+
+/**
+ * Created by nickitaliano on 1/8/18.
+ */
+
+public class UserLocationLayerConstants {
+    public static final String BACKGROUND_LAYER_ID = "mapbox-location-stroke-layer";
+    public static final String FOREGROUND_LAYER_ID = "mapbox-location-layer";
+    public static final String ACCURACY_LAYER_ID = "mapbox-location-accuracy-layer";
+    public static final String BEARING_LAYER_ID = "mapbox-location-bearing-layer";
+    public static final String NAVIGATION_LAYER_ID = "mapbox-location-navigation-layer";
+}
