ef79a02579fc0ac9eef9fee1cfb6b59e5a1fa1bf
Kristian Fallrø <kristfal@me.com>
2019-06-06 22:04:42 +0200

    7.0.0-rc2-release make pack script ignore package version

--- example/scripts/npm_pack_rngl.sh
+++ example/scripts/npm_pack_rngl.sh
@@ -4,4 +4,9 @@ echo "Moving into ${RNGL}"
 cd ../
 
 echo "Attempting to pack react-native-mapbox-gl"
-npm pack
+
+name=$(npm pack)
+
+echo "Renaming $name"
+
+mv "$name" "react-native-mapbox-gl-maps.tgz" 
