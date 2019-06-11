f8de53a03285e0b8e36548e6b045d17f4d62c6b2
Dheeraj Yalamanchili <Ausus@Ausus-MBP001.local>
2019-06-11 08:19:41 +0530

    adds HeatmapLayer support to iOS [WIP]

---
+++ ios/RCTMGL/RCTMGLHeatmapLayer.m
@@ -0,0 +1,49 @@
+//
+//  RCTMGLHeatmapLayer.m
+//  RCTMGL
+//
+//  Created by Dheeraj Yalamanchili on 6/8/2019
+
+#import "RCTMGLHeatmapLayer.h"
+#import "RCTMGLStyle.h"
+
+@implementation RCTMGLHeatmapLayer
+
+- (void)updateFilter:(NSPredicate *)predicate
+{
+    ((MGLHeatmapStyleLayer *) self.styleLayer).predicate = predicate;
+}
+
+- (void)setSourceLayerID:(NSString *)sourceLayerID
+{
+    _sourceLayerID = sourceLayerID;
+    
+    if (self.styleLayer != nil) {
+        ((MGLHeatmapStyleLayer*) self.styleLayer).sourceLayerIdentifier = _sourceLayerID;
+    }
+}
+
+- (void)addedToMap
+{
+    NSPredicate *filter = [self buildFilters];
+    if (filter != nil) {
+        [self updateFilter:filter];
+    }
+}
+
+- (MGLHeatmapStyleLayer*)makeLayer:(MGLStyle*)style
+{
+    MGLSource *source = [style sourceWithIdentifier:self.sourceID];
+    MGLHeatmapStyleLayer *layer = [[MGLHeatmapStyleLayer alloc] initWithIdentifier:self.id source:source];
+    layer.sourceLayerIdentifier = _sourceLayerID;
+    return layer;
+}
+
+- (void)addStyles
+{
+    RCTMGLStyle *style = [[RCTMGLStyle alloc] initWithMGLStyle:self.style];
+    style.bridge = self.bridge;
+    [style heatmapLayer:(MGLHeatmapStyleLayer *) withReactStyle:self.reactStyle];
+}
+
+@end
---
+++ ios/RCTMGL/RCTMGLHeatmapLayerManager.h
@@ -0,0 +1,12 @@
+//
+//  RCTMGLHeatmapLayerManager.h
+//  Pods
+//
+//  Created by Dheeraj Yalamanchili on 6/8/19.
+//
+
+#ifndef RCTMGLHeatmapLayerManager_h
+#define RCTMGLHeatmapLayerManager_h
+
+
+#endif /* RCTMGLHeatmapLayerManager_h */
