798050783e30c5999210353d2af83dbe43367a26
Damian Sznajder <dsznajder@users.noreply.github.com>
2019-07-26 10:01:48 +0200

    Update react-native.config.js

--- react-native.config.js
+++ react-native.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-  project: {
-    android: {
-      sourceDir: './android/rctmgl',
+  dependency: {
+    platforms: {
+      android: {
+        sourceDir: './android/rctmgl',
+      },
     },
   },
 }
