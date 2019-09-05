da9103183f77fc682b79315984f7d5c0f16594cb
doomsower <K.Kuznetcov@gmail.com>
2019-09-05 11:36:15 +0300

    fix: prop types

--- javascript/components/MapView.js
+++ javascript/components/MapView.js
@@ -106,6 +106,16 @@ class MapView extends NativeBridgeComponent(React.Component) {
     attributionEnabled: PropTypes.bool,
 
     /**
+     * Adds attribution offset, e.g. `{top: 8, left: 8}` will put attribution button in top-left corner of the map
+     */
+    attributionPosition: PropTypes.oneOfType([
+      PropTypes.shape({top: PropTypes.number, left: PropTypes.number}),
+      PropTypes.shape({top: PropTypes.number, right: PropTypes.number}),
+      PropTypes.shape({bottom: PropTypes.number, left: PropTypes.number}),
+      PropTypes.shape({bottom: PropTypes.number, right: PropTypes.number}),
+    ]),
+
+    /**
      * Enable/Disable the logo on the map.
      */
     logoEnabled: PropTypes.bool,
