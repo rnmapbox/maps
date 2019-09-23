0f76fde3c5015562c163634cdf8472f33b81bb69
Richard Lindhout <richardlindhout96@gmail.com>
2019-09-23 20:52:47 +0200

    Update index.d.ts (#405)

--- index.d.ts
+++ index.d.ts
@@ -147,7 +147,7 @@ declare namespace MapboxGL {
         Light = 'mapbox://styles/mapbox/light-v10',
         Outdoors = 'mapbox://styles/mapbox/outdoors-v11',
         Satellite = 'mapbox://styles/mapbox/satellite-v9',
-        SatelliteStreeet = 'mapbox://styles/mapbox/satellite-streets-v11',
+        SatelliteStreet = 'mapbox://styles/mapbox/satellite-streets-v11',
         TrafficDay = 'mapbox://styles/mapbox/navigation-preview-day-v4',
         TrafficNight = 'mapbox://styles/mapbox/navigation-preview-night-v4'
     }
