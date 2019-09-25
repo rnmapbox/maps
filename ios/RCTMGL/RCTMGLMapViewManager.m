3d413d9e4c789357127e626f955a219ca9b12cc5
Kristian Fallrø <kristfal@me.com>
2019-09-25 10:47:43 +0200

    Fix iOS programatic region did change and add react native config (#413)
    * Fix region did change and add react native config

--- ios/RCTMGL/RCTMGLMapViewManager.m
+++ ios/RCTMGL/RCTMGLMapViewManager.m
@@ -442,8 +442,8 @@ RCT_EXPORT_METHOD(showAttribution:(nonnull NSNumber *)reactTag
 }
 
 - (void)mapView:(MGLMapView *)mapView regionDidChangeWithReason:(MGLCameraChangeReason)reason animated:(BOOL)animated
-{
-    if (reason & MGLCameraChangeReasonTransitionCancelled == MGLCameraChangeReasonTransitionCancelled) return;
+{    
+    if ((reason & MGLCameraChangeReasonTransitionCancelled) == MGLCameraChangeReasonTransitionCancelled) return;
 
     ((RCTMGLMapView *) mapView).isUserInteraction = (BOOL)(reason & ~MGLCameraChangeReasonProgrammatic);
     
