f0f9e80ba808f74d8912f89b7154cc5fb7cf33e9
Your Name <you@example.com>
2020-02-23 17:24:44 +0100

    Add #_handleCameraChange tests

--- __tests__/components/Camera.test.js
+++ __tests__/components/Camera.test.js
@@ -3,6 +3,49 @@ import {render} from 'react-native-testing-library';
 
 import Camera from '../../javascript/components/Camera';
 
+const cameraWithoutFollowDefault = {
+  animationDuration: 2000,
+  animationMode: 'easeTo',
+  centerCoordinate: [-111.8678, 40.2866],
+  zoomLevel: 16,
+  followUserLocation: false,
+  followUserMode: 'normal',
+  isUserInteraction: false,
+};
+
+const cameraWithoutFollowChanged = {
+  animationDuration: 1000,
+  animationMode: 'easeTo',
+  centerCoordinate: [-110.8678, 37.2866],
+  zoomLevel: 13,
+  followUserLocation: false,
+  followUserMode: 'normal',
+  isUserInteraction: false,
+};
+
+const cameraWithFollowCourse = {
+  animationDuration: 2000,
+  animationMode: 'easeTo',
+  defaultSettings: {
+    centerCoordinate: [-111.8678, 40.2866],
+    zoomLevel: 16,
+  },
+  followUserLocation: true,
+  followUserMode: 'course',
+  isUserInteraction: false,
+};
+
+const cameraWithBounds = {
+  animationDuration: 2000,
+  animationMode: 'easeTo',
+  bounds: {
+    ne: [-74.12641, 40.797968],
+    sw: [-74.143727, 40.772177],
+  },
+  isUserInteraction: false,
+  maxZoomLevel: 19,
+};
+
 describe('Camera', () => {
   test('renders', () => {
     const {getByTestId} = render(<Camera />);
@@ -32,6 +75,116 @@ describe('Camera', () => {
       maxBounds: null,
       defaultStop: null,
       onUserTrackingModeChange: undefined,
+    });
+  });
+
+  describe('methods', () => {
+    describe('#_handleCameraChange', () => {
+      let camera;
+
+      beforeEach(() => {
+        camera = new Camera();
+
+        // set up fake ref
+        camera.refs = {
+          camera: {
+            setNativeProps: jest.fn(),
+          },
+        };
+
+        // set up fake props
+        // we only do this, because we want to test the class methods!
+        camera.props = {};
+
+        jest.spyOn(camera, '_setCamera');
+        jest.spyOn(camera, '_hasCameraChanged');
+        jest.spyOn(camera, '_hasBoundsChanged');
+      });
+
+      test('does not call "#_setCamera" or "#setNativeProps" when `nextCamera` has no changes to `currentCamera`', () => {
+        camera._handleCameraChange(
+          cameraWithoutFollowDefault,
+          cameraWithoutFollowDefault,
+        );
+
+        expect(camera._hasCameraChanged).toHaveBeenCalled();
+        expect(camera._setCamera).not.toHaveBeenCalled();
+        expect(camera.refs.camera.setNativeProps).not.toHaveBeenCalled();
+      });
+
+      test('sets `followUserLocation` to false when it was removed on `nextCamera`', () => {
+        camera._handleCameraChange(
+          cameraWithFollowCourse,
+          cameraWithoutFollowDefault,
+        );
+
+        expect(camera._hasCameraChanged).toHaveBeenCalled();
+        expect(camera._setCamera).not.toHaveBeenCalled();
+
+        expect(camera.refs.camera.setNativeProps).toHaveBeenCalledTimes(1);
+
+        expect(camera.refs.camera.setNativeProps).toHaveBeenCalledWith({
+          followUserLocation: false,
+        });
+      });
+
+      test('sets `followUserLocation` to true when it was added on `nextCamera`', () => {
+        camera._handleCameraChange(
+          cameraWithoutFollowDefault,
+          cameraWithFollowCourse,
+        );
+
+        expect(camera._hasCameraChanged).toHaveBeenCalled();
+        expect(camera._setCamera).not.toHaveBeenCalled();
+
+        expect(camera.refs.camera.setNativeProps).toHaveBeenCalledTimes(2);
+
+        expect(camera.refs.camera.setNativeProps).toHaveBeenNthCalledWith(1, {
+          followUserLocation: true,
+        });
+        expect(camera.refs.camera.setNativeProps).toHaveBeenNthCalledWith(2, {
+          followHeading: undefined,
+          followPitch: undefined,
+          followUserMode: 'course',
+          followZoomLevel: undefined,
+        });
+      });
+
+      test('calls `#_setCamera` when `nextCamera` "hasChanged" without bounds', () => {
+        camera._handleCameraChange(
+          cameraWithoutFollowDefault,
+          cameraWithoutFollowChanged,
+        );
+
+        expect(camera._hasCameraChanged).toHaveBeenCalled();
+        expect(camera._hasBoundsChanged).not.toHaveBeenCalled();
+        expect(camera._setCamera).toHaveBeenCalledWith({
+          animationDuration: 1000,
+          animationMode: 'easeTo',
+          centerCoordinate: [-110.8678, 37.2866],
+          heading: undefined,
+          pitch: undefined,
+          zoomLevel: 13,
+        });
+      });
+
+      test('calls `#_hasBoundsChanged` & `#_setCamera` when `nextCamera` "hasChanged" with bounds', () => {
+        camera._handleCameraChange(
+          cameraWithoutFollowDefault,
+          cameraWithBounds,
+        );
+
+        expect(camera._hasCameraChanged).toHaveBeenCalled();
+        expect(camera._hasBoundsChanged).toHaveBeenCalledTimes(1);
+        expect(camera._setCamera).toHaveBeenCalledWith({
+          animationDuration: 2000,
+          animationMode: 'easeTo',
+          bounds: {ne: [-74.12641, 40.797968], sw: [-74.143727, 40.772177]},
+          heading: undefined,
+          pitch: undefined,
+          zoomLevel: undefined,
+        });
+      });
     });
   });
 });
