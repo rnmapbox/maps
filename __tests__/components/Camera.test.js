d2968b81055b57aba334d7a2d8af6ce4bb8eb6ee
Your Name <you@example.com>
2020-02-28 14:51:18 +0100

    linting

--- __tests__/components/Camera.test.js
+++ __tests__/components/Camera.test.js
@@ -48,37 +48,36 @@ const cameraWithBounds = {
 
 describe('Camera', () => {
   describe('render', () => {
-  test('renders correctly', () => {
-    const {getByTestId} = render(<Camera />);
+    test('renders correctly', () => {
+      const {getByTestId} = render(<Camera />);
 
-    expect(getByTestId('Camera')).toBeDefined();
-  });
-
-  test('has proper default props', () => {
-    const {getByTestId} = render(<Camera />);
-
-    expect(getByTestId('Camera').props).toStrictEqual({
-      testID: 'Camera',
-      followUserLocation: undefined,
-      followUserMode: undefined,
-      followUserPitch: undefined,
-      followHeading: undefined,
-      followZoomLevel: undefined,
-      stop: {
-        mode: 'Ease',
-        pitch: undefined,
-        heading: undefined,
-        duration: 2000,
-        zoom: undefined,
-      },
-      maxZoomLevel: undefined,
-      minZoomLevel: undefined,
-      maxBounds: null,
-      defaultStop: null,
-      onUserTrackingModeChange: undefined,
+      expect(getByTestId('Camera')).toBeDefined();
     });
-  });
 
+    test('has proper default props', () => {
+      const {getByTestId} = render(<Camera />);
+
+      expect(getByTestId('Camera').props).toStrictEqual({
+        testID: 'Camera',
+        followUserLocation: undefined,
+        followUserMode: undefined,
+        followUserPitch: undefined,
+        followHeading: undefined,
+        followZoomLevel: undefined,
+        stop: {
+          mode: 'Ease',
+          pitch: undefined,
+          heading: undefined,
+          duration: 2000,
+          zoom: undefined,
+        },
+        maxZoomLevel: undefined,
+        minZoomLevel: undefined,
+        maxBounds: null,
+        defaultStop: null,
+        onUserTrackingModeChange: undefined,
+      });
+    });
   });
 
   describe('class', () => {
