3bdcc9096eae981a5cc9911f03e8c42a42fa991b
Miklos Fazekas <mfazekas@szemafor.com>
2020-04-07 10:12:27 +0200

    Upgrade example to RN 0.62.1

--- example/src/App.js
+++ example/src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import MapboxGL from '@react-native-mapbox-gl/maps';
-import {StyleSheet, Text, View, YellowBox} from 'react-native';
-import SafeAreaView from 'react-native-safe-area-view';
-import {createStackNavigator} from 'react-navigation';
-import CardStackStyleInterpolator from 'react-navigation-stack/dist/views/StackView/StackViewStyleInterpolator'; // eslint-disable-line import/no-extraneous-dependencies
+import {StyleSheet, Text, View, YellowBox, SafeAreaView} from 'react-native';
+import {createStackNavigator, TransitionPresets} from 'react-navigation-stack';
+import {createAppContainer} from 'react-navigation';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import sheet from './styles/sheet';
@@ -40,14 +39,11 @@ const AppStackNavigator = createStackNavigator(
 
     navigationOptions: {
       header: null,
-    },
-
-    transitionConfig: () => ({
-      screenInterpolator: props =>
-        CardStackStyleInterpolator.forVertical(props),
-    }),
+      ...TransitionPresets.SlideFromRightIOS,
+    }
   },
 );
+const AppContainer = createAppContainer(AppStackNavigator);
 
 class App extends React.Component {
   constructor(props) {
@@ -88,7 +84,7 @@ class App extends React.Component {
         </SafeAreaView>
       );
     }
-    return <AppStackNavigator />;
+    return <AppContainer />;
   }
 }
 
