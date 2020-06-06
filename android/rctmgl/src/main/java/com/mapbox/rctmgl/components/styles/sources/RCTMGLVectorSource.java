a630f6ed321a176952fbc4142e21c5b581470b2c
Antony Agrios <antonyagrios@gmail.com>
2020-06-06 11:48:25 +0300

    fix(RCTMLGLVectourSource): Fix a bug where mSource being null blew up the app

--- android/rctmgl/src/main/java/com/mapbox/rctmgl/components/styles/sources/RCTMGLVectorSource.java
+++ android/rctmgl/src/main/java/com/mapbox/rctmgl/components/styles/sources/RCTMGLVectorSource.java
@@ -47,11 +47,13 @@ public class RCTMGLVectorSource extends RCTMGLTileSource<VectorSource> {
     public void querySourceFeatures(String callbackID,
                                              @Size(min = 1) List<String> layerIDs,
                                              @Nullable Expression filter) {
-        List<Feature> features = mSource.querySourceFeatures(layerIDs.toArray(new String[layerIDs.size()]), filter);
-        WritableMap payload = new WritableNativeMap();
-        payload.putString("data", FeatureCollection.fromFeatures(features).toJson());
+        if(mSource != null){
+            List<Feature> features = mSource.querySourceFeatures(layerIDs.toArray(new String[layerIDs.size()]), filter);
+            WritableMap payload = new WritableNativeMap();
+            payload.putString("data", FeatureCollection.fromFeatures(features).toJson());
 
-        AndroidCallbackEvent event = new AndroidCallbackEvent(this, callbackID, payload);
-        mManager.handleEvent(event);
+            AndroidCallbackEvent event = new AndroidCallbackEvent(this, callbackID, payload);
+            mManager.handleEvent(event);
+        }
     }
 }
