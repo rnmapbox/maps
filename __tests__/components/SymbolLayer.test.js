d55f1c551b3dcf42c31ba0a906caf5e9e6fe3245
Tom Daniel <tomdaniel0x01@gmail.com>
2020-08-14 03:44:32 -0600

    Added test for SymbolLayer (#980)
    * Added test for SymbolLayer
    
    * Removed unnecessary test.
    
    Co-authored-by: Tom Daniel <tom.daniel@informu.io>

---
+++ __tests__/components/SymbolLayer.test.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {render} from 'react-native-testing-library';
+import PropTypes from 'prop-types';
+
+import SymbolLayer, {
+  NATIVE_MODULE_NAME,
+} from '../../javascript/components/SymbolLayer';
+
+describe('SymbolLayer', () => {
+  test('renders correctly with default props', () => {
+    const {UNSAFE_getByType} = render(<SymbolLayer />);
+    const symbolLayer = UNSAFE_getByType(NATIVE_MODULE_NAME);
+    const {props} = symbolLayer;
+
+    expect(props.sourceID).toStrictEqual('DefaultSourceID');
+  });
+
+  test('renders correctly with custom props', () => {
+    const customProps = {
+      id: 'customId',
+      sourceID: 'customSourceId',
+      sourceLayerID: 'customSourceLayerId',
+      aboveLayerID: 'customAboveLayerId',
+      belowLayerID: 'customBelowLayerId',
+      layerIndex: 0,
+      filter: ['==', 'arbitraryFilter', true],
+      minZoomLevel: 3,
+      maxZoomLevel: 8,
+      style: {visibility: 'none'},
+    };
+
+    const {UNSAFE_getByType} = render(<SymbolLayer {...customProps} />);
+    const symbolLayer = UNSAFE_getByType(NATIVE_MODULE_NAME);
+    const {props} = symbolLayer;
+
+    expect(props.id).toStrictEqual(customProps.id);
+    expect(props.sourceID).toStrictEqual(customProps.sourceID);
+    expect(props.sourceLayerID).toStrictEqual(customProps.sourceLayerID);
+    expect(props.aboveLayerID).toStrictEqual(customProps.aboveLayerID);
+    expect(props.belowLayerID).toStrictEqual(customProps.belowLayerID);
+    expect(props.layerIndex).toStrictEqual(customProps.layerIndex);
+    expect(props.filter).toStrictEqual(customProps.filter);
+    expect(props.minZoomLevel).toStrictEqual(customProps.minZoomLevel);
+    expect(props.maxZoomLevel).toStrictEqual(customProps.maxZoomLevel);
+    expect(props.reactStyle).toStrictEqual({
+      visibility: {
+        styletype: 'constant',
+        stylevalue: {type: 'string', value: customProps.style.visibility},
+      },
+    });
+  });
+});
