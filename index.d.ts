5888e31b05f721ae88c26fbff450690fd0a7b5d7
Jonas <jonas.altin@gmail.com>
2021-02-11 10:20:19 +0100

    Update index.d.ts
    Fixed a type

--- index.d.ts
+++ index.d.ts
@@ -210,7 +210,7 @@ declare namespace MapboxGL {
      * The heading (measured in degrees) relative to true north.
      * Heading is used to describe the direction the device is pointing to (the value of the compass).
      * Note that on Android this is incorrectly reporting the course value as mentioned in issue https://github.com/react-native-mapbox-gl/maps/issues/1213
-     * and will be corrected in a feature update.
+     * and will be corrected in a future update.
      */
     heading?: number;
 
