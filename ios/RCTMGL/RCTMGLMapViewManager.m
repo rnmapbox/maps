9199f959fd4936071ab0d120caabd11fba06bb07
Miklós Fazekas <mfazekas@szemafor.com>
2021-03-10 12:39:14 +0100

    Support onPRess and onMapChange

--- ios/RCTMGL/RCTMGLMapViewManager.m
+++ ios/RCTMGL/RCTMGLMapViewManager.m
@@ -3,4 +3,6 @@
 
 @interface RCT_EXTERN_MODULE(RCTMGLMapViewManager, RCTViewManager)
 RCT_REMAP_VIEW_PROPERTY(styleURL, reactStyleURL, NSString)
+RCT_REMAP_VIEW_PROPERTY(onPress, reactOnPress, RCTBubblingEventBlock)
+RCT_REMAP_VIEW_PROPERTY(onMapChange, reactOnMapChange, RCTBubblingEventBlock)
 @end
