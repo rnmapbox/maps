3d4592c2c5859a775bccba353a0caf6d496210f0
Kid Commit <26439946+ferdicus@users.noreply.github.com>
2021-03-12 13:04:01 +0100

    add detox (#1267)

---
+++ example/e2e/firstTest.e2e.js
@@ -0,0 +1,15 @@
+describe('Maps Example App', () => {
+  beforeAll(async () => {
+    await device.launchApp();
+  });
+
+  afterEach(async () => {
+    await device.reloadReactNative();
+  });
+
+  it('should show initial screen', async () => {
+    await expect(element(by.text('Map'))).toBeVisible();
+    await expect(element(by.text('Camera'))).toBeVisible();
+    await expect(element(by.text('User Location'))).toBeVisible();
+  });
+});
