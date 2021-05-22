64b8063ffa5ad124ba249159eb1df14a933d3a52
ferdicus <26439946+ferdicus@users.noreply.github.com>
2021-05-22 23:22:22 +0200

    add nvm ios fix

---
+++ example/scripts/fix_nvm_issue.js
@@ -0,0 +1,19 @@
+// fix ios build issue related to nvm
+// error message: `nvm is not compatible with the "PREFIX" environment variable: currently set to "/usr/local"`
+// see detailed discussion here => https://github.com/facebook/react-native/issues/31181
+// see detailed discussion here => https://github.com/facebook/react-native/issues/31259
+
+const fs = require('fs');
+
+// solution as described here: https://github.com/facebook/react-native/issues/31181#issuecomment-815913541
+const anchorLine = /set -e/;
+const replacementContent = 'unset npm_config_prefix\nunset PREFIX\nset -e\n';
+const problemFilePath = './node_modules/react-native/scripts/find-node.sh';
+const problemFileContent = fs.readFileSync(problemFilePath, 'utf8');
+fs.writeFileSync(
+  problemFilePath,
+  problemFileContent.replace(anchorLine, replacementContent),
+  'utf8'
+);
+
+console.log('🙏🏻 nvm with iOS should work 🙏🏻');
