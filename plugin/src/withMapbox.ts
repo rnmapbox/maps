a001ad67ddff3b980cfd56e42defd0723caa5f4e
Evan Bacon <baconbrix@gmail.com>
2021-08-09 19:33:41 -0600

    fix: exclude arm64 architectures for simulator builds

--- plugin/src/withMapbox.ts
+++ plugin/src/withMapbox.ts
@@ -5,6 +5,8 @@ import {
   ConfigPlugin,
   createRunOncePlugin,
   withDangerousMod,
+  withXcodeProject,
+  XcodeProject,
 } from '@expo/config-plugins';
 import {
   mergeContents,
@@ -104,7 +106,33 @@ export function addMapboxInstallerBlock(
   }).contents;
 }
 
+/**
+ * Exclude building for arm64 on simulator devices in the pbxproj project.
+ * Without this, production builds targeting simulators will fail.
+ */
+export function setExcludedArchitectures(project: XcodeProject): XcodeProject {
+  const configurations = project.pbxXCBuildConfigurationSection();
+  // @ts-ignore
+  for (const {buildSettings} of Object.values(configurations || {})) {
+    // Guessing that this is the best way to emulate Xcode.
+    // Using `project.addToBuildSettings` modifies too many targets.
+    if (typeof buildSettings?.PRODUCT_NAME !== 'undefined') {
+      buildSettings['"EXCLUDED_ARCHS[sdk=iphonesimulator*]"'] = '"arm64"';
+    }
+  }
+
+  return project;
+}
+
+const withExcludedSimulatorArchitectures: ConfigPlugin = (c) => {
+  return withXcodeProject(c, (config) => {
+    config.modResults = setExcludedArchitectures(config.modResults);
+    return config;
+  });
+};
+
 const withMapbox: ConfigPlugin = (config) => {
+  config = withExcludedSimulatorArchitectures(config);
   return withCocoaPodsInstallerBlocks(config);
 };
 
