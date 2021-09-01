66beba8796058951ad0adbd0c4d1db7ad7b61447
Miklós Fazekas <mfazekas@szemafor.com>
2021-09-01 19:00:38 +0200

    allow geometry as shape

--- ios/RCTMGL-v10/RCTMGLCircleLayer.swift
+++ ios/RCTMGL-v10/RCTMGLCircleLayer.swift
@@ -29,10 +29,11 @@ class RCTMGLCircleLayer: RCTMGLVectorLayer {
     if let style : Style = self.style {
       let styler =  RCTMGLStyle(style: self.style!)
       styler.bridge = self.bridge
-      if var styleLayer = self.styleLayer as? LayerType {
+      if var styleLayer = self.styleLayer as? LayerType,
+         let reactStyle = self.reactStyle {
         styler.circleLayer(
           layer: &styleLayer,
-          reactStyle: reactStyle!,
+          reactStyle: reactStyle,
           applyUpdater: { (updater) in try! style.updateLayer(withId: self.id) { (layer: inout LayerType) in updater(&layer) }},
           isValid: { return self.isAddedToMap() })
         self.styleLayer = styleLayer
