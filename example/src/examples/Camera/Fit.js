85b42496cbae7d69f313ff8919d94c75b2601b79
Naftali Beder <naftali@naftalibeder.com>
2021-11-01 15:54:01 -0500

    Remove duplicate prop

--- example/src/examples/Camera/Fit.js
+++ example/src/examples/Camera/Fit.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { isEqual } from 'lodash';
+import {View, Text} from 'react-native';
+import {isEqual} from 'lodash';
 import MapboxGL from '@react-native-mapbox-gl/maps';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
 import sheet from '../../styles/sheet';
 import BaseExamplePropTypes from '../common/BaseExamplePropTypes';
-import { TouchableOpacity } from 'react-native-gesture-handler';
 import Page from '../common/Page';
 
 const buildPadding = ([top, right, bottom, left] = [0, 0, 0, 0]) => {
@@ -27,8 +27,14 @@ const townBounds = {
   sw: [-74.143727, 40.772177],
 };
 
-const houseCenter = [(houseBounds.ne[0] + houseBounds.sw[0]) / 2, (houseBounds.ne[1] + houseBounds.sw[1]) / 2];
-const townCenter = [(townBounds.ne[0] + townBounds.sw[0]) / 2, (townBounds.ne[1] + townBounds.sw[1]) / 2];
+const houseCenter = [
+  (houseBounds.ne[0] + houseBounds.sw[0]) / 2,
+  (houseBounds.ne[1] + houseBounds.sw[1]) / 2,
+];
+const townCenter = [
+  (townBounds.ne[0] + townBounds.sw[0]) / 2,
+  (townBounds.ne[1] + townBounds.sw[1]) / 2,
+];
 
 class Fit extends React.Component {
   static propTypes = {...BaseExamplePropTypes};
@@ -49,24 +55,37 @@ class Fit extends React.Component {
     return (
       <View style={{paddingBottom: 5, opacity: fade ? 0.5 : 1}}>
         <Text>{title}</Text>
-        <View style={{flex: 0, flexDirection: 'row', width: '100%', paddingVertical: 10}}>
+        <View
+          style={{
+            flex: 0,
+            flexDirection: 'row',
+            width: '100%',
+            paddingVertical: 10,
+          }}>
           {buttons.map(button => (
             <TouchableOpacity
               key={`${button.value}`}
-              style={{flex: 0, padding: 5, marginRight: 5, backgroundColor: isEqual(this.state[key], button.value) ? 'coral' : '#d8d8d8', borderRadius: 5}}
-              key={button.title}
-              onPress={() => this.setState({ [key]: button.value })}
-            >
+              style={{
+                flex: 0,
+                padding: 5,
+                marginRight: 5,
+                backgroundColor: isEqual(this.state[key], button.value)
+                  ? 'coral'
+                  : '#d8d8d8',
+                borderRadius: 5,
+              }}
+              onPress={() => this.setState({[key]: button.value})}>
               <Text>{button.title}</Text>
             </TouchableOpacity>
           ))}
         </View>
       </View>
-    )
-  }
+    );
+  };
 
   render() {
-    const { fitType, containType, zoomLevel, padding, animationDuration } = this.state;
+    const {fitType, containType, zoomLevel, padding, animationDuration} =
+      this.state;
 
     let cameraProps = {
       bounds: undefined,
@@ -78,7 +97,8 @@ class Fit extends React.Component {
     if (fitType === 'bounds') {
       cameraProps.bounds = containType === 'house' ? houseBounds : townBounds;
     } else if (fitType === 'centerCoordinate') {
-      cameraProps.centerCoordinate = containType === 'house' ? houseCenter : townCenter;
+      cameraProps.centerCoordinate =
+        containType === 'house' ? houseCenter : townCenter;
     }
     if (zoomLevel !== undefined) {
       cameraProps.zoomLevel = zoomLevel;
@@ -95,22 +115,35 @@ class Fit extends React.Component {
           </View>
         </MapboxGL.MapView>
 
-        <View style={{flex: 0, width: '100%', padding: 10, paddingBottom: 20, backgroundColor: 'white'}}>
+        <View
+          style={{
+            flex: 0,
+            width: '100%',
+            padding: 10,
+            paddingBottom: 20,
+            backgroundColor: 'white',
+          }}>
           {this.renderSection('Fit Type', 'fitType', [
-            { title: 'Bounds', value: 'bounds' },
-            { title: 'Center Coordinate', value: 'centerCoordinate' },
+            {title: 'Bounds', value: 'bounds'},
+            {title: 'Center Coordinate', value: 'centerCoordinate'},
           ])}
           {this.renderSection('Contain Type', 'containType', [
-            { title: 'House', value: 'house' },
-            { title: 'Town', value: 'town' },
+            {title: 'House', value: 'house'},
+            {title: 'Town', value: 'town'},
           ])}
-          {this.renderSection('Zoom' + (fitType === 'bounds' ? ' (Not used because bounds is set)' : ''), 'zoomLevel', [undefined, 14, 15, 16, 17, 18, 19, 20].map(n => {
-            return { title: `${n}`, value: n };
-          }), fitType === 'bounds')}
+          {this.renderSection(
+            'Zoom' +
+              (fitType === 'bounds' ? ' (Not used because bounds is set)' : ''),
+            'zoomLevel',
+            [undefined, 14, 15, 16, 17, 18, 19, 20].map(n => {
+              return {title: `${n}`, value: n};
+            }),
+            fitType === 'bounds',
+          )}
           {this.renderSection('Padding', 'padding', [
-            { title: 'None', value: buildPadding() },
-            { title: 'Top', value: buildPadding([200, 40, 40, 40]) },
-            { title: 'Bottom', value: buildPadding([40, 40, 200, 40]) },
+            {title: 'None', value: buildPadding()},
+            {title: 'Top', value: buildPadding([200, 40, 40, 40])},
+            {title: 'Bottom', value: buildPadding([40, 40, 200, 40])},
           ])}
         </View>
       </Page>
