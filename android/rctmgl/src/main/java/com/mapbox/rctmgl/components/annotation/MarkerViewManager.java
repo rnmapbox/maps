ae57cfb630081d0d22f6261e6e7d5b0366a0d765
Ufuk Bozkurt <ufukbozkurt@Ufuks-MacBook-Pro.local>
2022-02-04 13:14:36 +0300

    fix(android):update MarkerView position on camera move

--- android/rctmgl/src/main/java/com/mapbox/rctmgl/components/annotation/MarkerViewManager.java
+++ android/rctmgl/src/main/java/com/mapbox/rctmgl/components/annotation/MarkerViewManager.java
@@ -8,17 +8,26 @@ import com.mapbox.mapboxsdk.maps.MapboxMap;
 import java.util.ArrayList;
 import java.util.List;
 
+import java.lang.reflect.InvocationTargetException;
+
 /**
  * Subclass of MarkerViewManager implementing removeViews and restoreViews
  */
 public class MarkerViewManager extends com.mapbox.mapboxsdk.plugins.markerview.MarkerViewManager {
     private final List<MarkerView> markers = new ArrayList<>();
     private MapView mapView;
+    private java.lang.reflect.Method markerUpdate;
 
     public MarkerViewManager(MapView mapView, MapboxMap mapboxMap) {
         super(mapView, mapboxMap);
         this.mapView = mapView;
         // this.mapboxMap = mapboxMap;
+
+        try {     
+            markerUpdate = MarkerView.class.getSuperclass().getDeclaredMethod("update");
+            markerUpdate.setAccessible(true);
+        } 
+        catch (NoSuchMethodException e) {System.out.println(e.toString());}
     }
 
     public void addMarker(@NonNull MarkerView markerView) {
@@ -42,4 +51,17 @@ public class MarkerViewManager extends com.mapbox.mapboxsdk.plugins.markerview.M
             mapView.addView(marker.getView());
         }
     }
+
+    public void updateMarkers(){
+
+        try {          
+            for( int i = 0; i < markers.size(); i++ ){
+                markerUpdate.invoke(markers.get(i));
+            }
+        } 
+        catch (IllegalArgumentException e) { System.out.println(e.toString()); }
+        catch (IllegalAccessException e) { System.out.println(e.toString()); }
+        catch (InvocationTargetException e) { System.out.println(e.toString()); }
+    }
+
 }
