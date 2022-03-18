6b621840539f7bbb3437fd98e09686091d1aad68
Jelle Breuer <jelle@breuclan.nl>
2022-03-18 10:01:50 +0100

    Fix MapLibre with mapbox maps

--- android/rctmgl/src/main/java-maplibre/com/mapbox/rctmgl/impl/InstanceManagerImpl.java
+++ android/rctmgl/src/main/java-maplibre/com/mapbox/rctmgl/impl/InstanceManagerImpl.java
@@ -2,10 +2,11 @@ package com.mapbox.rctmgl.impl;
 
 import android.content.Context;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.WellKnownTileServer;
 
 public class InstanceManagerImpl {
   public static void getInstance(Context context, String accessToken) {
-    Mapbox.getInstance(context);
+    Mapbox.getInstance(context, accessToken, WellKnownTileServer.Mapbox);
   }
 
   public static String getAccessToken() {
