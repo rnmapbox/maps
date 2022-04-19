a0123d7bc752eaca6eb28d285f32901644cc43f7
Naftali Beder <naftali@naftalibeder.com>
2022-04-19 14:42:18 -0500

    Incorporate tap and long-press into example

--- example/src/examples/V10/MapGestureHandlers.js
+++ example/src/examples/V10/MapGestureHandlers.js
@@ -1,9 +1,16 @@
 import React, {useState} from 'react';
 import {SafeAreaView, View} from 'react-native';
-import {MapView, Camera, Logger} from '@rnmapbox/maps';
+import {
+  MapView,
+  Camera,
+  CircleLayer,
+  ShapeSource,
+  Logger,
+} from '@rnmapbox/maps';
 import {Text, Divider} from 'react-native-elements';
 
 import Page from '../common/Page';
+import colors from '../../styles/colors';
 
 Logger.setLogLevel('verbose');
 
@@ -26,18 +33,32 @@ const styles = {
 const MapGestureHandlers = props => {
   const [lastCallback, setLastCallback] = useState('');
   const [region, setRegion] = useState({});
+  const [features, setFeatures] = useState([]);
 
   const properties = region?.properties;
+
+  const buildShape = feature => {
+    return {
+      type: 'Point',
+      coordinates: feature.geometry.coordinates,
+    };
+  };
+
+  const addFeature = (feature, kind) => {
+    const _feature = {...feature};
+    _feature.properties.kind = kind;
+    setFeatures(prev => [...prev, _feature]);
+  };
 
   return (
     <Page {...props}>
       <MapView
         style={styles.map}
-        onPress={e => {
-          console.log(e);
+        onPress={_feature => {
+          addFeature(_feature, 'press');
         }}
-        onLongPress={e => {
-          console.log(e);
+        onLongPress={_feature => {
+          addFeature(_feature, 'longPress');
         }}
         onCameraChanged={_region => {
           setLastCallback('onCameraChanged');
@@ -52,10 +73,34 @@ const MapGestureHandlers = props => {
           zoomLevel={12}
           animationDuration={0}
         />
+        {features.map((f, i) => {
+          const id = JSON.stringify(f.geometry.coordinates);
+          const circleStyle =
+            f.properties.kind === 'press'
+              ? {
+                  circleColor: colors.primary.blue,
+                  circleRadius: 6,
+                }
+              : {
+                  circleColor: colors.primary.pink,
+                  circleRadius: 12,
+                };
+          return (
+            <ShapeSource key={id} id={`source-${id}`} shape={buildShape(f)}>
+              <CircleLayer id={`layer-${id}`} style={circleStyle} />
+            </ShapeSource>
+          );
+        })}
       </MapView>
 
       <SafeAreaView>
         <View style={styles.info}>
+          <Text style={styles.fadedText}>
+            Tap or long-press to create a marker.
+          </Text>
+
+          <Divider style={styles.divider} />
+
           <Text style={styles.fadedText}>lastCallback</Text>
           <Text>{lastCallback}</Text>
 
