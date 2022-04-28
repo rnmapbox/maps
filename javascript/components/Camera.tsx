bfe8dcff1f64fee5c31426bdf24dc2f217330809
Naftali Beder <naftali@naftalibeder.com>
2022-04-28 15:16:10 -0500

    Export prop types

--- javascript/components/Camera.tsx
+++ javascript/components/Camera.tsx
@@ -64,7 +64,7 @@ type UserTrackingModeChangeCallback = (
  * @param {number} maxZoomLevel
  * @param {CameraBounds} maxBounds
  */
-interface CameraProps
+export interface CameraProps
   extends Omit<CameraStop, 'type'>,
     CameraFollowConfig,
     CameraMinMaxConfig {
