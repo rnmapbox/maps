6d498bea6e31d7a5315075d554b208464b7dd72b
Naftali Beder <naftali@naftalibeder.com>
2022-05-17 13:54:51 -0500

    Fix build

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,40 +2,35 @@ import { Component } from 'react';
 
 import { InterpolationMode, NamedStyles } from '..';
 
-declare namespace MapboxGL {
-  export function removeCustomHeader(headerName: string): void;
-  export function addCustomHeader(
-    headerName: string,
-    headerValue: string,
+export function removeCustomHeader(headerName: string): void;
+export function addCustomHeader(headerName: string, headerValue: string): void;
+export function setAccessToken(accessToken: string | null): void;
+export function getAccessToken(): Promise<string>;
+export function setTelemetryEnabled(telemetryEnabled: boolean): void;
+export function setConnected(connected: boolean): void;
+
+export class StyleSheet extends Component {
+  static create<T extends NamedStyles<T> | NamedStyles<any>>(styles: T): T;
+  camera(
+    stops: { [key: number]: string },
+    interpolationMode?: InterpolationMode,
   ): void;
-  export function setAccessToken(accessToken: string | null): void;
-  export function getAccessToken(): Promise<string>;
-  export function setTelemetryEnabled(telemetryEnabled: boolean): void;
-  export function setConnected(connected: boolean): void;
+  source(
+    stops: { [key: number]: string },
+    attributeName: string,
+    interpolationMode?: InterpolationMode,
+  ): void;
+  composite(
+    stops: { [key: number]: string },
+    attributeName: string,
+    interpolationMode?: InterpolationMode,
+  ): void;
 
-  export class StyleSheet extends Component {
-    static create<T extends NamedStyles<T> | NamedStyles<any>>(styles: T): T;
-    camera(
-      stops: { [key: number]: string },
-      interpolationMode?: InterpolationMode,
-    ): void;
-    source(
-      stops: { [key: number]: string },
-      attributeName: string,
-      interpolationMode?: InterpolationMode,
-    ): void;
-    composite(
-      stops: { [key: number]: string },
-      attributeName: string,
-      interpolationMode?: InterpolationMode,
-    ): void;
+  identity(attributeName: string): number;
+}
 
-    identity(attributeName: string): number;
-  }
-
-  export class OfflinePack extends Component {
-    status(): void;
-    resume(): void;
-    pause(): void;
-  }
+export class OfflinePack extends Component {
+  status(): void;
+  resume(): void;
+  pause(): void;
 }
