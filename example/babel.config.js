44a508c0fe940d369542b28639067258eadaa756
Miklós Fazekas <mfazekas@szemafor.com>
2022-07-06 08:00:40 +0200

    chore(example): Use babel config instead of metro to resolve lib in example app

--- example/babel.config.js
+++ example/babel.config.js
@@ -1,7 +1,44 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    '@babel/plugin-transform-modules-commonjs',
-    ['module-resolver'],
-  ].filter(Boolean),
+module.exports = (api) => {
+  const isWeb = api.caller(isTargetWeb);
+
+  return {
+    presets: [
+      '@babel/preset-typescript',
+      'module:metro-react-native-babel-preset',
+    ],
+    plugins: [
+      '@babel/plugin-proposal-optional-chaining',
+      '@babel/plugin-transform-modules-commonjs',
+      isWeb
+        ? [
+            'module-resolver',
+            {
+              alias: {
+                '@rnmapbox/maps': './maps/',
+              },
+            },
+          ]
+        : [
+            'module-resolver',
+            {
+              alias: {
+                '@rnmapbox/maps': '../javascript',
+                react: './node_modules/react',
+                'react-native': './node_modules/react-native',
+                '@babel': './node_modules/@babel',
+                '@turf': './node_modules/@turf',
+                '@mapbox': './node_modules/@mapbox',
+                debounce: './node_modules/debounce',
+                'prop-types': './node_modules/prop-types',
+                'deprecated-react-native-prop-types':
+                  './node_modules/deprecated-react-native-prop-types',
+              },
+            },
+          ],
+    ].filter(Boolean),
+  };
 };
+
+function isTargetWeb(caller) {
+  return caller && caller.name === 'babel-loader';
+}
