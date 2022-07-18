728b082cef2c7a4770479e2871ae9be8aceb9c53
Polyrion <polyrion.inc@gmail.com>
2022-07-18 13:53:48 +0200

    fix: add deprecated-react-native-prop-types to patch viewproptypes issue due to his removal from rn >= 69.0

--- javascript/utils/index.js
+++ javascript/utils/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {
-  ViewPropTypes,
   View,
   NativeModules,
   findNodeHandle,
   Platform,
 } from 'react-native';
 import resolveAssetSource from 'react-native/Libraries/Image/resolveAssetSource';
+import { ViewPropTypes } from 'deprecated-react-native-prop-types';
 
 function getAndroidManagerInstance(module) {
   const haveViewManagerConfig =
