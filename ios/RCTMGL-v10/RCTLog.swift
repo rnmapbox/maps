ab03363cd5039e2461e8b237adfbcdbc962cbad3
Craig Martin <cmart1220@gmail.com>
2022-07-21 15:29:55 -0400

    Resolve RCTSwiftLog duplicate symbols with other plugins when use_frameworks! enabled (#2055)

--- ios/RCTMGL-v10/RCTLog.swift
+++ ios/RCTMGL-v10/RCTLog.swift
@@ -1,19 +1,19 @@
 func RCTLogError(_ message: String, _ file: String=#file, _ line: UInt=#line) {
-  RCTSwiftLog.error(message, file: file, line: line)
+  RCTMGLSwiftLog.error(message, file: file, line: line)
 }
 
 func RCTLogWarn(_ message: String, _ file: String=#file, _ line: UInt=#line) {
-  RCTSwiftLog.warn(message, file: file, line: line)
+  RCTMGLSwiftLog.warn(message, file: file, line: line)
 }
 
 func RCTLogInfo(_ message: String, _ file: String=#file, _ line: UInt=#line) {
-  RCTSwiftLog.info(message, file: file, line: line)
+  RCTMGLSwiftLog.info(message, file: file, line: line)
 }
 
 func RCTLog(_ message: String, _ file: String=#file, _ line: UInt=#line) {
-  RCTSwiftLog.log(message, file: file, line: line)
+  RCTMGLSwiftLog.log(message, file: file, line: line)
 }
 
 func RCTLogTrace(_ message: String, _ file: String=#file, _ line: UInt=#line) {
-  RCTSwiftLog.trace(message, file: file, line: line)
+  RCTMGLSwiftLog.trace(message, file: file, line: line)
 }
--- ios/RCTMGL-v10/RCTSwiftLog.h
+++ ios/RCTMGL-v10/RCTSwiftLog.h
@@ -1,6 +1,6 @@
 #import <Foundation/Foundation.h>
 
-@interface RCTSwiftLog : NSObject
+@interface RCTMGLSwiftLog : NSObject
 
 + (void)error:(NSString * _Nonnull)message file:(NSString * _Nonnull)file line:(NSUInteger)line;
 + (void)warn:(NSString * _Nonnull)message file:(NSString * _Nonnull)file line:(NSUInteger)line;
