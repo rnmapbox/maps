3a2ebd386296bdc24b8bd14ec68ac88f33ee8dd7
Craig Martin <cmart1220@gmail.com>
2022-07-25 16:32:02 -0400

    Set defaults and run yarn generate

--- javascript/components/MapView.js
+++ javascript/components/MapView.js
@@ -145,7 +145,7 @@ class MapView extends NativeBridgeComponent(React.Component) {
     compassEnabled: PropTypes.bool,
 
     /**
-     * [`mapbox` (v10) implementation only] Enable/Disable whether the compass fades out when the map is pointing north
+     * [`mapbox` (v10) implementation only] Enable/Disable if the compass should fade out when the map is pointing north
      */
     compassFadeWhenNorth: PropTypes.bool,
 
@@ -306,6 +306,8 @@ class MapView extends NativeBridgeComponent(React.Component) {
     pitchEnabled: true,
     rotateEnabled: true,
     attributionEnabled: true,
+    compassEnabled: false,
+    compassFadeWhenNorth: false,
     logoEnabled: true,
     scaleBarEnabled: true,
     surfaceView: false,
