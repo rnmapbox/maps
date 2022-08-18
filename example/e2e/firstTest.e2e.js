59bb087a080fd05c1a29732f7979dfe29234ccf0
Miklós Fazekas <mfazekas@szemafor.com>
2022-08-18 19:36:43 +0200

    chore(e2e): detox test handle location permission, click map to show location

--- example/e2e/firstTest.e2e.js
+++ example/e2e/firstTest.e2e.js
@@ -1,6 +1,6 @@
 describe('Maps Example App', () => {
   beforeAll(async () => {
-    await device.launchApp();
+    await device.launchApp({ permissions: { location: 'always' } });
   });
 
   afterEach(async () => {
@@ -18,5 +18,19 @@ describe('Maps Example App', () => {
     await element(by.text('Map')).tap();
     await expect(element(by.text('Show Map'))).toBeVisible();
     await element(by.text('Show Map')).tap();
+    await expect(element(by.text('Light'))).toBeVisible();
+    await element(by.text('Light')).tap();
+  });
+
+  it('should show click location', async () => {
+    await expect(element(by.text('Map'))).toBeVisible();
+    await element(by.text('Map')).tap();
+    await expect(element(by.text('Show Click'))).toBeVisible();
+    await element(by.text('Show Click')).tap();
+    await expect(element(by.id('show-click-map-view'))).toBeVisible();
+    await element(by.id('show-click-map-view')).tap();
+    await waitFor(element(by.id('location-bubble-latitude')))
+      .toBeVisible()
+      .withTimeout(1000);
   });
 });
--- example/src/examples/common/Bubble.js
+++ example/src/examples/common/Bubble.js
@@ -22,14 +22,16 @@ class Bubble extends React.PureComponent {
     onPress: PropTypes.func,
     children: PropTypes.any,
     style: PropTypes.any,
+    testID: PropTypes.string,
   };
 
   render() {
     let innerChildView = this.props.children;
+    let { testID } = this.props;
 
     if (this.props.onPress) {
       innerChildView = (
-        <TouchableOpacity onPress={this.props.onPress}>
+        <TouchableOpacity onPress={this.props.onPress} testID={testID}>
           {this.props.children}
         </TouchableOpacity>
       );
