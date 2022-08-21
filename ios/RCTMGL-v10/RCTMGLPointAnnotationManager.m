ce75129852ad0f71edebb92d79e63832cbfda105
Craig Martin <cmart1220@gmail.com>
2022-08-21 02:13:47 -0400

    iOS V10 PointAnnotation Drag and Selection Support (#2077)
    * Feature: migrateOfflineCache
    
    - Added the migrate function for iOS and Android from the official Mapbox 'Migrate to v10' guides
    
    * iOS V10 PointAnnotation Build Out
    
    - Added draggable support for PointAnnotation
    - Added support for onDrag, onDragEnd, onDragStart, onDeselected, and onSelected
    
    * Update android/rctmgl/src/main/java-v10/com/mapbox/rctmgl/modules/RCTMGLOfflineModule.kt
    
    Co-authored-by: Miklós Fazekas <mfazekas@szemafor.com>
    
    * Update android/rctmgl/src/main/java-v10/com/mapbox/rctmgl/modules/RCTMGLOfflineModule.kt
    
    Co-authored-by: Miklós Fazekas <mfazekas@szemafor.com>
    
    * RCTMGLInteractiveComponent
    
    - Created new subclass RCTMGLInteractiveComponent that extracts commonality between RCTMGLSource and RCTMGLPointAnnotation
    
    * Renamed RCTMGLInteractiveComponent to RCTMGLInteractiveElement
    
    * Remove extra migrateOfflineCache method
    
    * Feature: migrateOfflineCache
    
    - Added the migrate function for iOS and Android from the official Mapbox 'Migrate to v10' guides
    
    * iOS V10 PointAnnotation Build Out
    
    - Added draggable support for PointAnnotation
    - Added support for onDrag, onDragEnd, onDragStart, onDeselected, and onSelected
    
    * Update android/rctmgl/src/main/java-v10/com/mapbox/rctmgl/modules/RCTMGLOfflineModule.kt
    
    Co-authored-by: Miklós Fazekas <mfazekas@szemafor.com>
    
    * Update android/rctmgl/src/main/java-v10/com/mapbox/rctmgl/modules/RCTMGLOfflineModule.kt
    
    Co-authored-by: Miklós Fazekas <mfazekas@szemafor.com>
    
    * RCTMGLInteractiveComponent
    
    - Created new subclass RCTMGLInteractiveComponent that extracts commonality between RCTMGLSource and RCTMGLPointAnnotation
    
    * Renamed RCTMGLInteractiveComponent to RCTMGLInteractiveElement
    
    * Remove extra migrateOfflineCache method
    
    * Remove extra isDraggable function from RCTMGLSource
    
    Co-authored-by: Miklós Fazekas <mfazekas@szemafor.com>

--- ios/RCTMGL-v10/RCTMGLPointAnnotationManager.m
+++ ios/RCTMGL-v10/RCTMGLPointAnnotationManager.m
@@ -5,6 +5,14 @@
 @interface RCT_EXTERN_REMAP_MODULE(RCTMGLPointAnnotation, RCTMGLPointAnnotationManager, RCTViewManager)
 
 RCT_EXPORT_VIEW_PROPERTY(coordinate, NSString)
+RCT_EXPORT_VIEW_PROPERTY(draggable, BOOL)
+RCT_EXPORT_VIEW_PROPERTY(id, NSString)
+
+RCT_REMAP_VIEW_PROPERTY(onMapboxPointAnnotationDeselected, onDeselected, RCTBubblingEventBlock)
+RCT_REMAP_VIEW_PROPERTY(onMapboxPointAnnotationDrag, onDrag, RCTBubblingEventBlock)
+RCT_REMAP_VIEW_PROPERTY(onMapboxPointAnnotationDragEnd, onDragEnd, RCTBubblingEventBlock)
+RCT_REMAP_VIEW_PROPERTY(onMapboxPointAnnotationDragStart, onDragStart, RCTBubblingEventBlock)
+RCT_REMAP_VIEW_PROPERTY(onMapboxPointAnnotationSelected, onSelected, RCTBubblingEventBlock)
 
 RCT_EXTERN_METHOD(refresh:(nonnull NSNumber*)reactTag
                   resolver:(RCTPromiseResolveBlock)resolve
