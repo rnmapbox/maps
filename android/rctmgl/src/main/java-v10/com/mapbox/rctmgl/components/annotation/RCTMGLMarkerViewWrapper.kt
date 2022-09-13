93c5f749a4b6258cc417b552a94f2d3c291e0cd8
Miklós Fazekas <mfazekas@szemafor.com>
2022-09-13 16:08:51 +0200

    fix(v10,android,markerview): Fix MarkerView flicker on resize (#2205)

---
+++ android/rctmgl/src/main/java-v10/com/mapbox/rctmgl/components/annotation/RCTMGLMarkerViewWrapper.kt
@@ -0,0 +1,8 @@
+package com.mapbox.rctmgl.components.annotation
+import android.content.Context
+import android.util.Log
+import android.view.View
+import com.facebook.react.views.view.ReactViewGroup
+
+class RCTMGLMarkerViewWrapper(context: Context?, private val mManager: RCTMGLMarkerViewWrapperManager) :  ReactViewGroup(context) {
+}
