9c2ed18a7933e0de8ac0fa56432d3f96ed223a94
Naftali Beder <naftali@naftalibeder.com>
2022-09-28 16:53:03 -0500

    Delete unnecessary wrapper component

--- android/rctmgl/src/main/java-v10/com/mapbox/rctmgl/components/annotation/RCTMGLMarkerViewWrapper.kt
+++
@@ -1,8 +0,0 @@
-package com.mapbox.rctmgl.components.annotation
-import android.content.Context
-import android.util.Log
-import android.view.View
-import com.facebook.react.views.view.ReactViewGroup
-
-class RCTMGLMarkerViewWrapper(context: Context?, private val mManager: RCTMGLMarkerViewWrapperManager) :  ReactViewGroup(context) {
-}
