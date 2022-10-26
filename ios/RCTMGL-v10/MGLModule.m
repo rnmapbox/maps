0e02923dab824ab4d1fe4c52d77e9d5e8bcf3372
Miklós Fazekas <mfazekas@szemafor.com>
2022-10-26 06:36:03 +0200

    feat(v10, ios): setTelemetryEnabled (#2331)

--- ios/RCTMGL-v10/MGLModule.m
+++ ios/RCTMGL-v10/MGLModule.m
@@ -7,4 +7,6 @@ RCT_EXTERN_METHOD(setAccessToken:)
 RCT_EXTERN_METHOD(addCustomHeader:(NSString *)headerName forHeaderValue:(NSString *) headerValue)
 RCT_EXTERN_METHOD(removeCustomHeader:(NSString *)headerName)
 
+RCT_EXTERN_METHOD(setTelemetryEnabled:(BOOL)telemetryEnabled )
+
 @end
