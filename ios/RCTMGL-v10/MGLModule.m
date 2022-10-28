769ad8aec71da1444cdbce08bc4e03a89549c335
Naftali Beder <naftali@naftalibeder.com>
2022-10-27 22:55:16 -0500

    Fix v10 interface signature (#2348)

--- ios/RCTMGL-v10/MGLModule.m
+++ ios/RCTMGL-v10/MGLModule.m
@@ -2,7 +2,7 @@
 
 @interface RCT_EXTERN_MODULE(MGLModule, NSObject)
 
-RCT_EXTERN_METHOD(setAccessToken:)
+RCT_EXTERN_METHOD(setAccessToken:(NSString *)accessToken resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 
 RCT_EXTERN_METHOD(addCustomHeader:(NSString *)headerName forHeaderValue:(NSString *) headerValue)
 RCT_EXTERN_METHOD(removeCustomHeader:(NSString *)headerName)
