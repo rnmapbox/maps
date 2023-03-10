8d9c3a184b9710aa118104b74f186322345ce3ca
Kilian Finger <finger@trafficon.eu>
2023-03-10 07:45:55 +0100

    chore: cleanup plugin code

--- plugin/build/withMapbox.d.ts
+++ plugin/build/withMapbox.d.ts
@@ -4,16 +4,16 @@ export declare type MapboxPlugProps = {
     RNMapboxMapsImpl?: string;
     RNMapboxMapsDownloadToken?: string;
 };
-export declare function applyCocoaPodsModifications(contents: string, { RNMapboxMapsImpl, RNMapboxMapsDownloadToken }: MapboxPlugProps): string;
-export declare function addConstantBlock(src: string, RNMapboxMapsImpl?: string, RNMapboxMapsDownloadToken?: string): string;
-export declare function addInstallerBlock(src: string, blockName: InstallerBlockName): string;
-export declare function addMapboxInstallerBlock(src: string, blockName: InstallerBlockName): string;
+export declare const addInstallerBlock: (src: string, blockName: InstallerBlockName) => string;
+export declare const addConstantBlock: (src: string, RNMapboxMapsImpl?: string, RNMapboxMapsDownloadToken?: string) => string;
+export declare const applyCocoaPodsModifications: (contents: string, { RNMapboxMapsImpl, RNMapboxMapsDownloadToken }: MapboxPlugProps) => string;
+export declare const addMapboxInstallerBlock: (src: string, blockName: InstallerBlockName) => string;
 /**
  * Exclude building for arm64 on simulator devices in the pbxproj project.
  * Without this, production builds targeting simulators will fail.
  */
-export declare function setExcludedArchitectures(project: XcodeProject): XcodeProject;
-export declare function addMapboxMavenRepo(src: string): string;
+export declare const setExcludedArchitectures: (project: XcodeProject) => XcodeProject;
+export declare const addMapboxMavenRepo: (src: string) => string;
 declare const _default: ConfigPlugin<MapboxPlugProps>;
 export default _default;
 export { addMapboxMavenRepo as _addMapboxMavenRepo, };
