1bab2cabb7a4a84ef6112f0c38c74fa3c9dd7318
Miklós Fazekas <mfazekas@szemafor.com>
2023-04-24 12:45:11 +0200

    fix(offline,android): fix getPackState on android (#2816)

--- ios/RCTMGL-v10/RCTMGLOfflineModule.swift
+++ ios/RCTMGL-v10/RCTMGLOfflineModule.swift
@@ -189,22 +189,31 @@ class RCTMGLOfflineModule: RCTEventEmitter {
   func getPackStatus(_ name: String,
                      resolver: @escaping RCTPromiseResolveBlock,
                      rejecter: @escaping RCTPromiseRejectBlock) {
-    guard let pack = tileRegionPacks[name] else {
+    guard tileRegionPacks[name] != nil else {
       rejecter("RCTMGLOfflineModule.getPackStatus", "pack \(name) not found", nil)
       return
     }
     
-    tileStore.tileRegionMetadata(forId: name) { result in
+    tileStore.tileRegion(forId: name) { result in
       switch result {
-      case .success(let metadata):
-        var pack = TileRegionPack(
-          name: name,
-          metadata: logged("RCTMGLOfflineModule.getPackStatus") { metadata as? [String:Any] } ?? [:]
-        )
-        self.tileRegionPacks[name] = pack
-        resolver(self._makeRegionStatusPayload(pack: pack))
+      case .success(let region):
+        self.tileStore.tileRegionMetadata(forId: name) { result in
+          switch result {
+          case .success(let metadata):
+            var pack = TileRegionPack(
+              name: name,
+              progress: self.toProgress(region: region),
+              metadata: logged("RCTMGLOfflineModule.getPackStatus") { metadata as? [String:Any] } ?? [:]
+            )
+            self.tileRegionPacks[name] = pack
+            resolver(self._makeRegionStatusPayload(pack: pack))
+          case .failure(let error):
+            Logger.log(level:.error, message: "Unable to fetch metadata for \(name)")
+            rejecter("RCTMGLOfflineModule.getPackStatus", error.localizedDescription, error)
+          }
+        }
       case .failure(let error):
-        Logger.log(level:.error, message: "Unable to fetch metadata for \(name)")
+        Logger.log(level:.error, message: "Unable to fetch region for \(name)")
         rejecter("RCTMGLOfflineModule.getPackStatus", error.localizedDescription, error)
       }
     }
