004eb0a3ec50a977727be379e13b62946ab302ca
Naftali Beder <naftali@naftalibeder.com>
2023-05-17 10:10:01 -0500

    Improve null checks

--- android/rctmgl/src/main/java-v10/com/mapbox/rctmgl/components/styles/sources/RCTMGLPointSource.kt
+++ android/rctmgl/src/main/java-v10/com/mapbox/rctmgl/components/styles/sources/RCTMGLPointSource.kt
@@ -1,25 +1,12 @@
 package com.mapbox.rctmgl.components.styles.sources
 
 import android.content.Context
+import android.util.Log
+import com.mapbox.bindgen.Value
 import com.mapbox.maps.extension.style.sources.generated.GeoJsonSource
-import com.mapbox.rctmgl.utils.ImageEntry
-import android.graphics.drawable.BitmapDrawable
-import com.facebook.react.bridge.ReadableMap
+import com.mapbox.maps.logD
 import com.mapbox.rctmgl.components.mapview.RCTMGLMapView
 import com.mapbox.rctmgl.events.FeatureClickEvent
-import com.facebook.react.bridge.WritableMap
-import com.facebook.react.bridge.WritableNativeMap
-import com.mapbox.bindgen.Value
-import com.mapbox.geojson.Feature
-import com.mapbox.rctmgl.events.AndroidCallbackEvent
-import com.mapbox.geojson.FeatureCollection
-import com.mapbox.geojson.Geometry
-import com.mapbox.maps.*
-import com.mapbox.maps.extension.style.expressions.generated.Expression
-import com.mapbox.rctmgl.utils.Logger
-import java.net.URL
-import java.util.ArrayList
-import java.util.HashMap
 
 class RCTMGLPointSource(context: Context, private val mManager: RCTMGLPointSourceManager) :
     RCTSource<GeoJsonSource>(context) {
@@ -45,24 +32,25 @@ class RCTMGLPointSource(context: Context, private val mManager: RCTMGLPointSourc
     override fun makeSource(): GeoJsonSource {
         val builder = GeoJsonSource.Builder(iD.toString())
 
-        if (mPoint != null) {
-            builder.data(mPoint!!)
+        val _mPoint = mPoint
+        if (_mPoint != null) {
+            builder.data(_mPoint)
         }
 
         return builder.build()
     }
 
-    fun updateStyle() {
-        val mSource = mSource ?: return
-        val mPoint = mPoint ?: return
-        val mMapView = mMapView ?: return
-        if (mMapView.isDestroyed) {
+    private fun updateStyle() {
+        val _mSource = mSource ?: return
+        val _mPoint = mPoint ?: return
+        val _mMapView = mMapView ?: return
+        if (_mMapView.isDestroyed) {
             return
         }
-        val mMap = mMap ?: return
+        val _mMap = mMap ?: return
 
-        mSource.data(mPoint)
-        mMap.getStyle()?.setStyleSourceProperty(iD!!, "data", Value.valueOf(mPoint))
+        _mSource.data(_mPoint)
+        _mMap.getStyle()?.setStyleSourceProperty(iD!!, "data", Value.valueOf(_mPoint))
     }
 
     override fun hasNoDataSoRefersToExisting(): Boolean {
