bf6ca747e98496f465e762fc2e5e5fd85b60c06b
Miklós Fazekas <mfazekas@szemafor.com>
2023-05-19 13:41:24 +0200

    chore: lint-code-example-in-issues ts support

--- scripts/actions/lint-code-example-in-issues/lint-code-example-in-issues.js
+++ scripts/actions/lint-code-example-in-issues/lint-code-example-in-issues.js
@@ -4,15 +4,143 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const { ESLint } = require('eslint');
 
+const Config = {
+  /// the login of the but used to make the comment
+  botLogin: 'github-actions[bot]',
+  /// the label to add when lint fails
+  label: 'error-in-code',
+  /// this is the marker is added to all comments made by this action
+  commentMarker: '<!-- lint-action -->',
+  /// the comment prefix to add when lint fails
+  commentPrelude:
+    '## Lint failed :sob:\n\nPlease fix the errors in your code example - [More info](https://github.com/rnmapbox/maps/wiki/ErrorInExamplesInIssue).:\n\n',
+  /// the comment to add when no code example is found
+  noCodeExampleComment:
+    'No code example found in issue body - [More info](https://github.com/rnmapbox/maps/wiki/ErrorInExamplesInIssue)',
+  /// whether to close the issue if no code example is found
+  closeIssueIfNoCodeExample: false,
+  /// whether to close the issue when lint fails
+  closeIssue: true,
+  /// whether to reopen the issue when lint passes
+  reopenIssue: true,
+  /// eslint formatter to use (stylish or codeframe is recommended)
+  formatter: 'codeframe',
+  closeLabel: 'reopen-on-code-fixed',
+  /// the eslint config used for javascript
+  eslintConfig: {
+    root: true,
+    env: {
+      browser: true,
+      es2021: true,
+    },
+    extends: 'plugin:react/recommended',
+    overrides: [],
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    settings: {
+      react: {
+        version: '17.0.2',
+      },
+    },
+    plugins: ['react', 'eslint-plugin-import'],
+    rules: {
+      'import/prefer-default-export': ['error'],
+      'no-undef': 'error',
+      'no-restricted-imports': [
+        'error',
+        {
+          patterns: [
+            {
+              group: ['./*', '../*', '!../assets/example.png'],
+              message:
+                'Repo example should complete - it should not use files from your project',
+            },
+            {
+              group: ['!react', '!react-native', '!@rnmapbox/maps'],
+              message: 'Should not use third-party libraries',
+            },
+          ],
+        },
+      ],
+    },
+  },
+  /// the eslint config used for typescript
+  eslintConfigForTypescript: {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    env: {
+      browser: true,
+      es2021: true,
+    },
+    extends: 'plugin:react/recommended',
+    overrides: [],
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    settings: {
+      react: {
+        version: '17.0.2',
+      },
+    },
+    plugins: ['react', 'eslint-plugin-import'],
+    rules: {
+      'import/prefer-default-export': ['error'],
+      'no-undef': 'error',
+      'no-restricted-imports': [
+        'error',
+        {
+          patterns: [
+            {
+              group: ['./*', '../*', '!../assets/example.png'],
+              message:
+                'Repo example should complete - it should not use files from your project',
+            },
+            {
+              group: ['!react', '!react-native', '!@rnmapbox/maps'],
+              message: 'Should not use third-party libraries',
+            },
+          ],
+        },
+      ],
+    },
+  },
+};
+
+function log(...args) {
+  console.log('=>', ...args);
+}
+
 async function run() {
   try {
     const issueNumber = getIssueNumber();
-    const code = getCode();
-    const eslint = new ESLint({ fix: false });
-    const results = await eslint.lintText(code);
-    const formatter = await eslint.loadFormatter('stylish');
+    const [code, { isTypescript = false }] = getCode();
+    if (!code) {
+      await processGithubIssue(
+        issueNumber,
+        Config.noCodeExampleComment,
+        false,
+        true,
+      );
+      return;
+    }
+    const eslint = new ESLint({
+      fix: false,
+      useEslintrc: false,
+      overrideConfig: isTypescript
+        ? Config.eslintConfigForTypescript
+        : Config.eslintConfig,
+    });
+    const results = await eslint.lintText(code, {
+      filePath: isTypescript ? 'example.tsx' : 'example.jsx',
+    });
+
+    const hasErrors = results.some((result) => result.errorCount > 0);
+    const formatter = await eslint.loadFormatter('codeframe');
     const message = formatter.format(results);
-    await postComment(issueNumber, message);
+    await processGithubIssue(issueNumber, message, hasErrors, false);
   } catch (error) {
     core.setFailed(error.message);
   }
@@ -32,37 +160,84 @@ function getCode() {
     throw new Error('Could not find issue in context');
   }
   const { body } = issue;
-  const start = body.search(/```(jsx?|tsx?|javascript)/);
+  const start = body.search(/```(jsx?|tsx?|javascript|typescript)/i);
   if (start < 0) {
-    throw new Error('Could not find code block in issue body');
+    return [null, null];
   }
   const end = body.indexOf('```', start + 1);
   const bodywithprefix = body.substring(start, end);
-  return bodywithprefix.replace(/^```(jsx?|tsx?|javascript)/, '');
+
+  const isTypescript = !!bodywithprefix.match(/```(tsx?|typescript)/i);
+
+  return [
+    bodywithprefix.replace(/^```(jsx?|tsx?|javascript|typescript)/i, ''),
+    { isTypescript },
+  ];
 }
 
-async function postComment(issueNumber, message) {
+async function processGithubIssue(
+  issueNumber,
+  message,
+  hasErrors,
+  missingCode,
+) {
   const token = core.getInput('repo-token') || process.env.GITHUB_TOKEN;
-  if (! token) {
-    console.error("No token found. Wanted to post message: ", message)
+  if (!token) {
+    console.error('No token found. Wanted to post message: ', message);
   }
   const octokit = github.getOctokit(token);
 
-  const marker = '<!-- lint-action -->';
-  const label = 'error-in-code';
-  const prelude =
-    '## Lint failed :sob:\n\nPlease fix the errors in your code example - [More info](https://github.com/rnmapbox/maps/wiki/ErrorInExamplesInIssue).:\n\n';
+  const {
+    commentMarker,
+    label,
+    commentPrelude,
+    closeIssue,
+    reopenIssue,
+    closeLabel,
+    noCodeExampleComment,
+    closeIssueIfNoCodeExample,
+  } = Config;
 
   const context = {
     owner: github.context.repo.owner,
     repo: github.context.repo.repo,
   };
-  await removeOldCommandFromPreviousRuns(octokit, context, marker, issueNumber);
+  await removeOldCommandFromPreviousRuns(
+    octokit,
+    context,
+    commentMarker,
+    issueNumber,
+  );
 
-  if (message.includes('error')) {
+  if (missingCode) {
+    if (noCodeExampleComment) {
+      await octokit.rest.issues.createComment({
+        issue_number: issueNumber,
+        body: commentMarker + '\n' + noCodeExampleComment,
+        ...context,
+      });
+    }
+    if (closeIssueIfNoCodeExample) {
+      await octokit.rest.issues.update({
+        issue_number: issueNumber,
+        state: 'closed',
+        ...context,
+      });
+      if (closeLabel) {
+        await octokit.rest.issues.addLabels({
+          issue_number: issueNumber,
+          labels: [closeLabel],
+          ...context,
+        });
+      }
+    }
+    return;
+  }
+  if (hasErrors) {
     await octokit.rest.issues.createComment({
       issue_number: issueNumber,
-      body: marker + '\n' + prelude + '```eslint\n'+ message + '```',
+      body:
+        commentMarker + '\n' + commentPrelude + '```eslint\n' + message + '```',
       ...context,
     });
 
@@ -71,6 +246,21 @@ async function postComment(issueNumber, message) {
       labels: [label],
       ...context,
     });
+
+    if (closeIssue) {
+      await octokit.rest.issues.update({
+        issue_number: issueNumber,
+        state: 'closed',
+        ...context,
+      });
+      if (closeLabel) {
+        await octokit.rest.issues.addLabels({
+          issue_number: issueNumber,
+          labels: [closeLabel],
+          ...context,
+        });
+      }
+    }
   } else {
     try {
       await octokit.rest.issues.removeLabel({
@@ -83,21 +273,39 @@ async function postComment(issueNumber, message) {
         throw error;
       }
     }
+    if (reopenIssue) {
+      await octokit.rest.issues.update({
+        issue_number: issueNumber,
+        state: 'open',
+        ...context,
+      });
+      if (closeLabel) {
+        await octokit.rest.issues.removeLabel({
+          issue_number: issueNumber,
+          name: closeLabel,
+          ...context,
+        });
+      }
+    }
   }
 }
 
-async function removeOldCommandFromPreviousRuns(octokit, context, marker, issueNumber) {
+async function removeOldCommandFromPreviousRuns(
+  octokit,
+  context,
+  marker,
+  issueNumber,
+) {
   const { data: comments } = await octokit.rest.issues.listComments({
     issue_number: issueNumber,
     ...context,
   });
   const oldComment = comments.find(
     (comment) =>
-      comment.user.login === 'github-actions[bot]' &&
-      comment.body.startsWith(marker),
+      comment.user.login === Config.botLogin && comment.body.startsWith(marker),
   );
   if (oldComment) {
-    console.log('=> removing old comment');
+    log('=> removing old comment');
     await octokit.rest.issues.deleteComment({
       comment_id: oldComment.id,
       ...context,
