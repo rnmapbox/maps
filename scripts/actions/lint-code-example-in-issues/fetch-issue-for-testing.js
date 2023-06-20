9bc0c8fc89de59439f9caab94472444c18da6b84
Miklós Fazekas <mfazekas@szemafor.com>
2023-06-20 14:35:11 +0200

    lint fixes (#2920)

--- scripts/actions/lint-code-example-in-issues/fetch-issue-for-testing.js
+++ scripts/actions/lint-code-example-in-issues/fetch-issue-for-testing.js
@@ -1,6 +1,6 @@
+const fs = require('fs');
 
 const { Octokit } = require('@octokit/core');
-const fs = require('fs');
 
 async function fetchIssue(owner, repo, issueNumber) {
   const octokit = new Octokit();
@@ -10,15 +10,15 @@ async function fetchIssue(owner, repo, issueNumber) {
       owner,
       repo,
       issue_number: issueNumber,
-    }
+    },
   );
   return response.data;
-};
+}
 
 async function run() {
   let issue = await fetchIssue('rnmapbox', 'maps', process.argv[2] || 2867);
   fs.writeFileSync('test-issue.json', JSON.stringify({ issue }, null, 2));
-  console.log("issuse: ", issue);
+  console.log('issuse: ', issue);
 }
 
 run();
