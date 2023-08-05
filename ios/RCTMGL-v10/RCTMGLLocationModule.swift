c7d0600dc0a68c4e4edcb810c798b3c367a50799
Miklós Fazekas <mfazekas@szemafor.com>
2023-08-05 20:08:04 +0200

    fix(ios,v10): location manager race condition with initial location update (#2986)

--- ios/RCTMGL-v10/RCTMGLLocationModule.swift
+++ ios/RCTMGL-v10/RCTMGLLocationModule.swift
@@ -42,6 +42,126 @@ protocol LocationProviderRCTMGLDelegate : AnyObject {
   func locationManager(_ locationManager: LocationProviderRCTMGL, didUpdateLocation: RCTMGLLocation)
 }
 
+class RCTMGLAppleLocationProvider: NSObject {
+    private var locationProvider: CLLocationManager
+    private var privateLocationProviderOptions: LocationOptions {
+        didSet {
+            locationProvider.distanceFilter = privateLocationProviderOptions.distanceFilter
+            locationProvider.desiredAccuracy = privateLocationProviderOptions.desiredAccuracy
+            locationProvider.activityType = privateLocationProviderOptions.activityType
+        }
+    }
+    private weak var delegate: LocationProviderDelegate?
+
+    public var headingOrientation: CLDeviceOrientation {
+        didSet { locationProvider.headingOrientation = headingOrientation }
+    }
+
+    public override init() {
+        locationProvider = CLLocationManager()
+        privateLocationProviderOptions = LocationOptions()
+        headingOrientation = locationProvider.headingOrientation
+        super.init()
+        locationProvider.delegate = self
+    }
+}
+
+extension RCTMGLAppleLocationProvider: LocationProvider {
+
+    public var locationProviderOptions: LocationOptions {
+        get { privateLocationProviderOptions }
+        set { privateLocationProviderOptions = newValue }
+    }
+
+    public var authorizationStatus: CLAuthorizationStatus {
+        if #available(iOS 14.0, *) {
+            return locationProvider.authorizationStatus
+        } else {
+            return CLLocationManager.authorizationStatus()
+        }
+    }
+
+    public var accuracyAuthorization: CLAccuracyAuthorization {
+        if #available(iOS 14.0, *) {
+            return locationProvider.accuracyAuthorization
+        } else {
+            return .fullAccuracy
+        }
+    }
+
+    public var heading: CLHeading? {
+        return locationProvider.heading
+    }
+
+    public func setDelegate(_ delegate: LocationProviderDelegate) {
+        self.delegate = delegate
+    }
+
+    public func requestAlwaysAuthorization() {
+        locationProvider.requestAlwaysAuthorization()
+    }
+
+    public func requestWhenInUseAuthorization() {
+        locationProvider.requestWhenInUseAuthorization()
+    }
+
+    @available(iOS 14.0, *)
+    public func requestTemporaryFullAccuracyAuthorization(withPurposeKey purposeKey: String) {
+        locationProvider.requestTemporaryFullAccuracyAuthorization(withPurposeKey: purposeKey)
+    }
+
+    public func startUpdatingLocation() {
+        locationProvider.startUpdatingLocation()
+    }
+
+    public func stopUpdatingLocation() {
+        locationProvider.stopUpdatingLocation()
+    }
+
+    public func startUpdatingHeading() {
+        locationProvider.startUpdatingHeading()
+    }
+
+    public func stopUpdatingHeading() {
+        locationProvider.stopUpdatingHeading()
+    }
+
+    public func dismissHeadingCalibrationDisplay() {
+        locationProvider.dismissHeadingCalibrationDisplay()
+    }
+}
+
+extension RCTMGLAppleLocationProvider: CLLocationManagerDelegate {
+    public func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        delegate?.locationProvider(self, didUpdateLocations: locations)
+    }
+
+    public func locationManager(_ manager: CLLocationManager, didUpdateHeading heading: CLHeading) {
+        delegate?.locationProvider(self, didUpdateHeading: heading)
+    }
+
+    public func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        delegate?.locationProvider(self, didFailWithError: error)
+    }
+
+    @available(iOS 14.0, *)
+    public func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        delegate?.locationProviderDidChangeAuthorization(self)
+    }
+
+    public func locationManagerShouldDisplayHeadingCalibration(_ manager: CLLocationManager) -> Bool {
+        guard let calibratingDelegate = delegate as? CalibratingLocationProviderDelegate else {
+            return false
+        }
+
+        return calibratingDelegate.locationProviderShouldDisplayHeadingCalibration(self)
+    }
+}
+
+internal protocol CalibratingLocationProviderDelegate: LocationProviderDelegate {
+    func locationProviderShouldDisplayHeadingCalibration(_ locationProvider: LocationProvider) -> Bool
+}
+
 /// LocationProviderRCTMGL listens to updates from a locationProvider and implements the LocationProvider interface itself
 /// So it can be source of Mapbox locationProduces (which updates location pluck and viewport if configured) as well as source to updates
 /// to RCTMGLLocationModules.
@@ -84,7 +204,7 @@ class LocationProviderRCTMGL : LocationProviderDelegate {
   }
 
   init() {
-    provider = AppleLocationProvider()
+    provider = RCTMGLAppleLocationProvider()
     provider.setDelegate(self)
   }
   
@@ -222,6 +342,17 @@ extension LocationProviderRCTMGL: LocationProvider {
   func setDelegate(_ delegate: LocationProviderDelegate) {
     provider.setDelegate(self)
     locationProviderDelage = delegate
+
+    if let lastLocation = lastKnownLocation {
+      DispatchQueue.main.async {
+        self.locationProviderDelage?.locationProvider(self, didUpdateLocations: [lastLocation])
+      }
+    }
+    if let lastHeading = lastKnownHeading {
+      DispatchQueue.main.async { [self] in
+        self.locationProviderDelage?.locationProvider(self, didUpdateHeading: lastHeading)
+      }
+    }
   }
   
   func requestAlwaysAuthorization() {
