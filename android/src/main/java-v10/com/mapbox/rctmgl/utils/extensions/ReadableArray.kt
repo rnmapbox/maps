8e36ba745e700076e19c3aa0d22b9d6c362eb2f6
Miklós Fazekas <mfazekas@szemafor.com>
2023-09-03 16:14:43 +0200

    fix(android): fix queryRenderedFeaturesInRect in case rect is not conventional format (#3030)

--- android/src/main/java-v10/com/mapbox/rctmgl/utils/extensions/ReadableArray.kt
+++ android/src/main/java-v10/com/mapbox/rctmgl/utils/extensions/ReadableArray.kt
@@ -1,9 +1,12 @@
 package com.mapbox.rctmgl.utils.extensions
 
+import android.graphics.RectF
 import com.facebook.react.bridge.ReadableArray
 import com.mapbox.geojson.Point
 import com.mapbox.maps.ScreenCoordinate
 import com.mapbox.rctmgl.utils.Logger
+import java.lang.Float.min
+import java.lang.Float.max
 
 fun ReadableArray.toCoordinate() : Point {
     if (this.size() != 2) {
@@ -20,4 +23,16 @@ fun ReadableArray.toScreenCoordinate() : ScreenCoordinate {
         Logger.e("ReadableArray.toCoordinate","Cannot convert $this to point, 2 coordinates are required")
     }
     return ScreenCoordinate(getDouble(0), getDouble(1))
+}
+
+fun ReadableArray.toRectF() : RectF? {
+    if (size() != 4) {
+        return null;
+    }
+    return RectF(
+        min(getDouble(3).toFloat(), getDouble(1).toFloat()),
+        min(getDouble(0).toFloat(), getDouble(2).toFloat()),
+        max(getDouble(3).toFloat(), getDouble(1).toFloat()),
+        max(getDouble(0).toFloat(), getDouble(2).toFloat())
+    )
 }
