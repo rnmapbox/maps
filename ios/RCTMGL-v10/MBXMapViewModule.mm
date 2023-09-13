103d39f8fa606f39e807e930ae7ed054989aafc3
Wojciech Lewicki <wojciech.lewicki@swmansion.com>
2023-09-13 12:55:39 +0200

    fix: dispatch mapview methods on uimanager queue

--- ios/RCTMGL-v10/MBXMapViewModule.mm
+++ ios/RCTMGL-v10/MBXMapViewModule.mm
@@ -26,9 +26,16 @@ RCT_EXPORT_MODULE();
 #endif // RCT_NEW_ARCH_ENABLED
 @synthesize bridge = _bridge;
 
-- (void)withMapView:(NSNumber*)viewRef block:(void (^)(MBXMapView *))block reject:(RCTPromiseRejectBlock)reject
+- (dispatch_queue_t)methodQueue
 {
-    void (^upperBlock)(void) = ^{
+  // It seems that due to how UIBlocks work with uiManager, we need to call the methods there
+  // for the blocks to be dispatched before the batch is completed
+  return RCTGetUIManagerQueue();
+}
+
+- (void)withMapView:(NSNumber*)viewRef block:(void (^)(MBXMapView *))block reject:(RCTPromiseRejectBlock)reject methodName:(NSString *)methodName
+{
+//    void (^upperBlock)(void) = ^{
 #ifdef RCT_NEW_ARCH_ENABLED
     [self.viewRegistry_DEPRECATED addUIBlock:^(RCTViewRegistry *viewRegistry) {
         MBXMapViewComponentView *componentView = [self.viewRegistry_DEPRECATED viewForReactTag:viewRef];
@@ -42,15 +49,16 @@ RCT_EXPORT_MODULE();
         if (view != nil) {
            block(view);
         } else {
-            reject(@"takeSnap", [NSString stringWithFormat:@"Unknown reactTag: %@", viewRef], nil);
+            reject(methodName, [NSString stringWithFormat:@"Unknown reactTag: %@", viewRef], nil);
         }
     }];
-    };
-      if (self.bridge) {
-        dispatch_async(RCTGetUIManagerQueue(), upperBlock);
-      } else {
-        dispatch_async(dispatch_get_main_queue(), upperBlock);
-      }
+//        [self.bridge performSelector:@selector(batchDidComplete) withObject:self.bridge];
+//    };
+//      if (self.bridge) {
+//        dispatch_async(RCTGetUIManagerQueue(), upperBlock);
+//      } else {
+//        dispatch_async(dispatch_get_main_queue(), upperBlock);
+//      }
 }
 
 
@@ -58,20 +66,20 @@ RCT_EXPORT_METHOD(takeSnap:(NSNumber*)viewRef writeToDisk:(BOOL)writeToDisk reso
 {
     [self withMapView:viewRef block:^(MBXMapView *view) {
         [MBXMapViewManager takeSnap:view writeToDisk:writeToDisk resolver:resolve];
-    } reject:reject];
+    } reject:reject methodName:@"takeSnap"];
 }
 
 RCT_EXPORT_METHOD(clearData:(NSNumber*)viewRef resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject) {
     [self withMapView:viewRef block:^(MBXMapView *view) {
         [MBXMapViewManager clearData:view resolver:resolve rejecter:reject];
-    } reject:reject];
+    } reject:reject methodName:@"clearData"];
 }
 
 
 RCT_EXPORT_METHOD(getCenter:(NSNumber*)viewRef resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject) {
     [self withMapView:viewRef block:^(MBXMapView *view) {
         [MBXMapViewManager getCenter:view resolver:resolve rejecter:reject];
-    } reject:reject];
+    } reject:reject methodName:@"getCenter"];
 }
 
 
@@ -81,69 +89,69 @@ RCT_EXPORT_METHOD(getCoordinateFromView:(NSNumber*)viewRef atPoint:(NSArray *)at
         NSNumber* b = [atPoint objectAtIndex:1];
         
         [MBXMapViewManager getCoordinateFromView:view atPoint:CGPointMake(a.floatValue, b.floatValue) resolver:resolve rejecter:reject];
-    } reject:reject];
+    } reject:reject methodName:@"getCoordinateFromView"];
 }
 
 
-RCT_EXPORT_METHOD(getPointInView:(NSNumber*)viewRef atCoordinate:(NSArray *)atCoordinate resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject) {
+RCT_EXPORT_METHOD(getPointInView:(nonnull NSNumber*)viewRef atCoordinate:(NSArray *)atCoordinate resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject) {
     [self withMapView:viewRef block:^(MBXMapView *view) {
         [MBXMapViewManager getPointInView:view atCoordinate:atCoordinate resolver:resolve rejecter:reject];
-    } reject:reject];
+    } reject:reject methodName:@"getPointInView"];
 }
 
 
 RCT_EXPORT_METHOD(getVisibleBounds:(NSNumber*)viewRef resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject) {
     [self withMapView:viewRef block:^(MBXMapView *view) {
         [MBXMapViewManager getVisibleBounds:view resolver:resolve];
-    } reject:reject];
+    } reject:reject methodName:@"getVisibleBounds"];
 }
 
 
 RCT_EXPORT_METHOD(getZoom:(NSNumber*)viewRef resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject) {
     [self withMapView:viewRef block:^(MBXMapView *view) {
         [MBXMapViewManager getZoom:view resolver:resolve rejecter:reject];
-    } reject:reject];
+    } reject:reject methodName:@"getZoom"];
 }
 
 
 RCT_EXPORT_METHOD(queryRenderedFeaturesAtPoint:(NSNumber*)viewRef atPoint:(NSArray *)atPoint withFilter:(NSArray *)withFilter withLayerIDs:(NSArray *)withLayerIDs resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject) {
     [self withMapView:viewRef block:^(MBXMapView *view) {
         [MBXMapViewManager queryRenderedFeaturesAtPoint:view atPoint:atPoint withFilter:withFilter withLayerIDs:withLayerIDs resolver:resolve rejecter:reject];
-    } reject:reject];
+    } reject:reject methodName:@"queryRenderedFeaturesAtPoint"];
 }
 
 
 RCT_EXPORT_METHOD(queryRenderedFeaturesInRect:(NSNumber*)viewRef withBBox:(NSArray *)withBBox withFilter:(NSArray *)withFilter withLayerIDs:(NSArray *)withLayerIDs resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject) {
     [self withMapView:viewRef block:^(MBXMapView *view) {
         [MBXMapViewManager queryRenderedFeaturesInRect:view withBBox:withBBox withFilter:withFilter withLayerIDs:withLayerIDs resolver:resolve rejecter:reject];
-    } reject:reject];
+    } reject:reject methodName:@"queryRenderedFeaturesInRect"];
 }
 
 
 RCT_EXPORT_METHOD(queryTerrainElevation:(NSNumber*)viewRef coordinates:(NSArray *)coordinates resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject) {
     [self withMapView:viewRef block:^(MBXMapView *view) {
         [MBXMapViewManager queryTerrainElevation:view coordinates:coordinates resolver:resolve rejecter:reject];
-    } reject:reject];
+    } reject:reject methodName:@"queryTerrainElevation"];
 }
 
 
 RCT_EXPORT_METHOD(setHandledMapChangedEvents:(nonnull NSNumber*)viewRef events:(NSArray *)events resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject) {
     [self withMapView:viewRef block:^(MBXMapView *view) {
         [MBXMapViewManager setHandledMapChangedEvents:view events:events resolver:resolve rejecter:reject];
-    } reject:reject];
+    } reject:reject methodName:@"setHandledMapChangedEvents"];
 }
 
 
 RCT_EXPORT_METHOD(setSourceVisibility:(NSNumber*)viewRef visible:(BOOL)visible sourceId:(NSString *)sourceId sourceLayerId:(NSString *)sourceLayerId resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject) {
     [self withMapView:viewRef block:^(MBXMapView *view) {
         [MBXMapViewManager setSourceVisibility:view visible:visible sourceId:sourceId sourceLayerId:sourceLayerId resolver:resolve rejecter:reject];
-    } reject:reject];
+    } reject:reject methodName:@"setSourceVisibility"];
 }
 
 RCT_EXPORT_METHOD(querySourceFeatures:(NSNumber*)viewRef sourceId:(NSString*)sourceId withFilter:(NSArray<id>*)withFilter withSourceLayerIDs:(NSArray<NSString*>*)withSourceLayerIDs resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject) {
     [self withMapView:viewRef block:^(MBXMapView *view) {
         [MBXMapViewManager querySourceFeatures:view withSourceId:sourceId withFilter:withFilter withSourceLayerIds:withSourceLayerIDs resolver:resolve rejecter:reject];
-    } reject:reject];
+    } reject:reject methodName:@"querySourceFeatures"];
 }
 
 
