41de1e2cf4a3e8e31895691eb208370da1a6a021
Miklós Fazekas <mfazekas@szemafor.com>
2023-10-03 15:57:13 +0200

    generate example docs

--- example/e2e/docScreenshots.e2e.js
+++ example/e2e/docScreenshots.e2e.js
@@ -1,10 +1,14 @@
 const { mkdir, copyFile } = require('fs/promises');
 const fs = require('fs');
+const path = require('path');
 const { execSync } = require('child_process');
 
 const { device } = require('detox');
 
 const rootDir = '..';
+
+const docsRoot = '/Users/boga/Work/OSS/RNMBGL/map-docs/';
+const exampleDocsRoot = path.join(docsRoot, 'docs/examples');
 
 const examples = JSON.parse(
   fs.readFileSync(`${rootDir}/docs/examples.json`, 'utf8'),
@@ -17,12 +21,20 @@ async function setSampleLocation() {
   execSync(`xcrun simctl location ${device.id} set ${latitude},${longitude}`);
 }
 
-async function saveImage(imageName) {
-  const imagePath = await device.takeScreenshot(imageName);
-  const destDir = `${rootDir}/docs/screenshots`;
-  const destPath = `${destDir}/${imageName}.png`;
+async function saveImage(basePath, suffix = null) {
+  const imagePath = await device.takeScreenshot(`example-${suffix || ''}`);
+  // const destDir = `${rootDir}/docs/screenshots`;
+  // const destPath = `${destDir}/${imageName}.png`;
+  const destDir = path.dirname(basePath);
   await mkdir(destDir, { recursive: true });
-  await copyFile(imagePath, destPath);
+  var fullDestPath = basePath;
+  if (suffix != null) {
+    fullDestPath = `${basePath}-${suffix}.png`;
+  } else {
+    fullDestPath = `${basePath}.png`;
+  }
+  await copyFile(imagePath, fullDestPath);
+  await execSync(`sips -Z 640 ${fullDestPath}`);
 }
 
 async function wait(ms) {
@@ -56,7 +68,29 @@ describe('Example screenshots', () => {
           console.log(' => before wait');
           await wait(1000);
           console.log('=> after wait');
-          await saveImage(example.title);
+          console.log('[***] x', example, JSON.stringify(example));
+          const dirs = example.fullPath.split('/').slice(4);
+          const nameWithExt = dirs.pop();
+
+          const groupDir = path.join(exampleDocsRoot, ...dirs);
+          fs.mkdirSync(groupDir, { recursive: true });
+          const basename = nameWithExt.split('.').slice(0, -1).join('.');
+
+          const destPath = path.join(groupDir, `${basename}.md`);
+
+          const destImgPath = path.join(groupDir, `${basename}.png`);
+
+          fs.writeFileSync(
+            destPath,
+            `
+${example.docs}
+
+![${example.title}](./${basename}.png)
+
+          `,
+          );
+
+          await saveImage(path.join(groupDir, basename));
         });
       });
     });
