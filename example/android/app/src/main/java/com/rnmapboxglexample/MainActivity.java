7daf18836fcc217ccd6a0ed5be154e70b2bb0f80
Miklós Fazekas <mfazekas@szemafor.com>
2023-10-22 17:18:59 +0200

    fix: defaultMapboxImpl on android typo, chore: refactor example to use autoload to load @rnmapbox/maps

--- example/android/app/src/main/java/com/rnmapboxglexample/MainActivity.java
+++ example/android/app/src/main/java/com/rnmapboxglexample/MainActivity.java
@@ -8,6 +8,7 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
 
 public class MainActivity extends ReactActivity {
+
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
