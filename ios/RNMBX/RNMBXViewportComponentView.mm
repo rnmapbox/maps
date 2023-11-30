7a7d7aecde7e7d51b8d125281e4345e63483686d
Miklós Fazekas <mfazekas@szemafor.com>
2023-11-30 07:59:51 +0100

    CI error

--- ios/RNMBX/RNMBXViewportComponentView.mm
+++ ios/RNMBX/RNMBXViewportComponentView.mm
@@ -14,6 +14,7 @@
 
 #import "RCTFollyConvert.h"
 
+#error "An error 2"
 
 // TODO: use generated RNMBXViewportEventEmitter, but need 0.73+ for dynamic support
 class RNMBXViewportEventEmitter : public facebook::react::ViewEventEmitter {
