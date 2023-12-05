30f65d9cb783da26d79806c06034275e6a27a605
Miklós Fazekas <mfazekas@szemafor.com>
2023-12-05 22:02:58 +0100

    fix(android): Ignore the onMeasure on MarkerView while adding it to the annotationManager (#3242)

--- android/src/main/java/com/rnmapbox/rnmbx/components/annotation/RNMBXMarkerViewContent.kt
+++ android/src/main/java/com/rnmapbox/rnmbx/components/annotation/RNMBXMarkerViewContent.kt
@@ -1,21 +1,29 @@
 package com.rnmapbox.rnmbx.components.annotation
 
 import android.content.Context
+import android.view.View.MeasureSpec
 import com.facebook.react.uimanager.MeasureSpecAssertions
 import com.facebook.react.views.view.ReactViewGroup
 
 class RNMBXMarkerViewContent(context: Context): ReactViewGroup(context) {
+
+    var inAdd: Boolean = false
     // see https://github.com/rnmapbox/maps/pull/3235
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        try {
+        if (inAdd) {
+            val w = if (widthMeasureSpec == 0) {
+                MeasureSpec.makeMeasureSpec(measuredWidth, MeasureSpec.EXACTLY)
+            } else {
+                widthMeasureSpec
+            };
+            val h = if (heightMeasureSpec == 0) {
+                MeasureSpec.makeMeasureSpec(measuredHeight, MeasureSpec.EXACTLY)
+            } else {
+                heightMeasureSpec
+            }
+            super.onMeasure(w, h)
+        } else {
             super.onMeasure(widthMeasureSpec, heightMeasureSpec)
-        } catch(e: IllegalStateException) {
-            val w = MeasureSpec.getSize(widthMeasureSpec)
-            val h = MeasureSpec.getSize(heightMeasureSpec)
-            setMeasuredDimension(
-                if (w == 0) measuredWidth else w,
-                if (h == 0) measuredHeight else h
-            )
         }
     }
 
