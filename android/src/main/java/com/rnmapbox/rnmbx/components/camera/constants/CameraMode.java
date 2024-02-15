1d700f23b2ceb7e92003e1372026a4f0da0d796b
Naftali Beder <naftali@naftalibeder.com>
2024-02-15 09:32:38 -0600

    Add "move" mode definition

--- android/src/main/java/com/rnmapbox/rnmbx/components/camera/constants/CameraMode.java
+++ android/src/main/java/com/rnmapbox/rnmbx/components/camera/constants/CameraMode.java
@@ -7,12 +7,13 @@ import java.lang.annotation.RetentionPolicy;
 
 public class CameraMode {
 
-    @IntDef({ FLIGHT, EASE, LINEAR, NONE })
+    @IntDef({ FLIGHT, EASE, LINEAR, MOVE, NONE })
     @Retention(RetentionPolicy.SOURCE)
     public @interface Mode {}
 
     public static final int FLIGHT = 1;
     public static final int EASE = 2;
     public static final int LINEAR = 3;
-    public static final int NONE = 4;
+    public static final int MOVE = 4;
+    public static final int NONE = 5;
 }
