503d5a0b3330e0c55543ac98df45908eea2f99d0
Miklós Fazekas <mfazekas@szemafor.com>
2024-02-22 15:10:00 +0100

    fix(v11,ios): implement filter and sourceId for v11 layers

--- ios/RNMBX/RNMBXFillLayer.swift
+++ ios/RNMBX/RNMBXFillLayer.swift
@@ -20,14 +20,60 @@ public class RNMBXFillLayer: RNMBXVectorLayer {
   override func layerType() -> Layer.Type {
     return LayerType.self
   }
-  
+
+// @{codepart-replace-start(LayerPropsCommon.codepart-swift.ejs,{layerType:"Fill"})}
+  func setCommonOptions(_ layer: inout FillLayer) -> Bool {
+    var changed = false
+
+    #if RNMBX_11
+    if let sourceLayerID = sourceLayerID {
+      layer.sourceLayer = sourceLayerID
+      changed = true
+    }
+
+    if let sourceID = sourceID {
+      if !(existingLayer && sourceID == DEFAULT_SOURCE_ID) && hasSource() {
+        layer.source = sourceID
+        changed = true
+      }
+    }
+
+    if let filter = filter, filter.count > 0 {
+      do {
+        let data = try JSONSerialization.data(withJSONObject: filter, options: .prettyPrinted)
+        let decodedExpression = try JSONDecoder().decode(Expression.self, from: data)
+        layer.filter = decodedExpression
+        changed = true
+      } catch {
+        Logger.log(level: .error, message: "parsing filters failed for layer \(optional: id): \(error.localizedDescription)")
+      }
+    }
+    #endif
+
+    return changed
+  }
+
+  override func setOptions(_ layer: inout Layer) {
+    if var actualLayer = layer as? LayerType {
+      if self.setCommonOptions(&actualLayer) {
+        layer = actualLayer
+      }
+    } else {
+      Logger.log(level: .error, message: "Expected layer type to be Fill but was \(type(of: layer))")
+    }
+  }
+
   override func apply(style : Style) throws {
     try style.updateLayer(withId: id, type: LayerType.self) { (layer : inout FillLayer) in
+      if self.styleLayer != nil {
+        self.setOptions(&self.styleLayer!)
+      }
       if let styleLayer = self.styleLayer as? LayerType {
         layer = styleLayer
       }
     }
   }
+// @{codepart-replace-end}
 
   override func addStyles() {
     if let style : Style = self.style,
