0743399e1e0bef238d8dde0b9afac6e9e603bc00
Miklós Fazekas <mfazekas@szemafor.com>
2024-09-05 11:49:16 +0200

    fix(android): android build on pre 0.75

--- android/src/main/rn-compat/rn75/com/rnmapbox/rnmbx/rncompat/ResourceDrawableIdHelper.kt
+++
@@ -1,7 +0,0 @@
-package com.rnmapbox.rnmbx.rncompat.resourcedrawableidhelper;
-
-import com.facebook.react.views.imagehelper.ResourceDrawableIdHelper
-
-public fun ResourceDrawableIdHelper.Companion.getInstance(): ResourceDrawableIdHelper {
-    return this.instance
-}
