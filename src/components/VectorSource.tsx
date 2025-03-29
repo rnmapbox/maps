b040834a321b21373f111fe4b847f14c4b2d57ba
seth <seth@fastmail.org>
2025-03-29 01:36:27 -0400

    update source instead

--- src/components/VectorSource.tsx
+++ src/components/VectorSource.tsx
@@ -48,7 +48,11 @@ interface Props {
   minZoomLevel?: number;
 
   /**
-   * An unsigned integer that specifies the maximum zoom level at which to display tiles from the source.
+   *
+   * An unsigned integer that specifies the maximum zoom level for which tiles
+   * are available, as in the TileJSON spec. Data from tiles at the maxzoom are
+   * used when displaying the map at higher zoom levels.
+   *
    * The value should be between 0 and 22, inclusive, and less than
    * minZoomLevel, if specified. The default value for this option is 22.
    */
