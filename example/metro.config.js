aeeb558cb4306049508bcf1eb647024d410fa9f6
Michel Couillard <mcouillard@hortau.com>
2025-08-15 16:04:36 -0400

    code review

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,8 +1,7 @@
 // https://github.com/callstack/react-native-builder-bob/blob/main/packages/create-react-native-library/templates/expo-library/example/metro.config.js
 const path = require('path');
 
-// expo/metro-config doesn't support rn 0.80 yet
-const { getDefaultConfig } = require('@react-native/metro-config');
+const { getDefaultConfig } = require('@expo/metro-config');
 const exclusionList = require('metro-config/src/defaults/exclusionList');
 const escape = require('escape-string-regexp');
 
