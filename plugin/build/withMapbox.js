0eba2ba44ebea0ac0fa337eafcf8b200508d268c
Miklós Fazekas <mfazekas@szemafor.com>
2025-10-05 05:14:57 +0200

    feat: use env variable RNMAPBOX_MAPS_DOWNLOAD_TOKEN  to provide download token

--- plugin/build/withMapbox.js
+++ plugin/build/withMapbox.js
@@ -61,6 +61,8 @@ const addConstantBlock = (src, { RNMapboxMapsImpl, RNMapboxMapsVersion, RNMapbox
     }
     const newSrc = [];
     if (RNMapboxMapsDownloadToken) {
+        console.warn('⚠️ RNMapbox: RNMapboxMapsDownloadToken is deprecated. Use RNMAPBOX_MAPS_DOWNLOAD_TOKEN environment variable instead.');
+        console.warn('⚠️ RNMapbox: This token will be part of your Podfile. Be careful about committing it to source control.');
         newSrc.push(`$RNMapboxMapsDownloadToken = '${RNMapboxMapsDownloadToken}'`);
     }
     if (RNMapboxMapsImpl) {
@@ -132,6 +134,8 @@ const withCocoaPodsInstallerBlocks = (config, { RNMapboxMapsImpl, RNMapboxMapsVe
 const withAndroidPropertiesDownloadToken = (config, { RNMapboxMapsDownloadToken }) => {
     const key = 'MAPBOX_DOWNLOADS_TOKEN';
     if (RNMapboxMapsDownloadToken) {
+        console.warn('⚠️ WARNING: RNMapboxMapsDownloadToken is deprecated. Use RNMAPBOX_MAPS_DOWNLOAD_TOKEN environment variable instead.');
+        console.warn('⚠️ WARNING: This token will be part of your gradle.properties. Be careful about committing it to source control.');
         return (0, config_plugins_1.withGradleProperties)(config, (exportedConfig) => {
             exportedConfig.modResults = exportedConfig.modResults.filter((item) => !(item.type === 'property' && item.key === key));
             exportedConfig.modResults.push({
