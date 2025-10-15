64b8fc593c97db9b07e706a5bc6b919acd2cf91f
Thomas Han <thomas.han@halter.co.nz>
2025-10-15 16:53:56 +1300

    - remove eslint config "no-useless-escape"

--- .eslintrc.js
+++ .eslintrc.js
@@ -73,7 +73,6 @@ module.exports = {
     'no-return-assign': 0,
     'no-underscore-dangle': [0],
     'no-await-in-loop': 0,
-    'no-useless-escape': ['error', { allowRegexCharacters: ['-'] }],
     'no-restricted-syntax': 0,
     'no-use-before-define': ['error', { functions: false }],
     'no-unused-expressions': ['error', { allowTaggedTemplates: true }],
