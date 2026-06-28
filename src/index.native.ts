6684306f380ea960a1aa601dfb918f5e77762242
BubbleTrouble14 <ronald.goedeke@outlook.com>
2026-06-28 17:22:32 -0300

    fix(types): use 'export type' for MBTilesSource re-export
    Upstream merge enabled verbatimModuleSyntax; MBTilesSource is an interface
    so it must be re-exported with 'export type' (TS1205).

--- src/index.native.ts
+++ src/index.native.ts
@@ -2,6 +2,7 @@ export * from './Mapbox.native';
 import * as Mapbox from './Mapbox.native';
 
 // Add export for MBTiles
-export { default as MBTiles, MBTilesSource } from './modules/MBTiles';
+export { default as MBTiles } from './modules/MBTiles';
+export type { MBTilesSource } from './modules/MBTiles';
 
 export default Mapbox;
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * from './Mapbox';
 import * as Mapbox from './Mapbox';
 
 // Add export for MBTiles
-export { default as MBTiles, MBTilesSource } from './modules/MBTiles';
+export { default as MBTiles } from './modules/MBTiles';
+export type { MBTilesSource } from './modules/MBTiles';
 
 export default Mapbox;
